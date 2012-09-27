040ce19e106cbb0a5317e56d8a18b9a5e8040303
Tom Ritchford <tom@swirly.com>
2012-09-27 18:30:49 +0000

    MakeTimerSpec complete

--- cpp/MakeTimerSpec.cpp
+++ cpp/MakeTimerSpec.cpp
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <iostream>
+
 #include "MakeTimerSpec.h"
 
 using namespace std;
@@ -6,6 +9,18 @@ namespace echomesh {
 
 itimerspec makeTimerSpec(const string& s) {
   itimerspec spec = {};
+  timespec* value = &spec.it_value;
+
+  char* endptr;
+  value->tv_sec = strtol(s.c_str(), &endptr, 10);
+  if (*endptr == '.') {
+    string t((string(endptr) + "000000000").substr(0, 9));
+    value->tv_nsec = strtol(t.c_str(), &endptr, 10);
+    if (*endptr)
+      cerr << "Didn't understand nanoseconds in time " << t;
+  } else if (*endptr) {
+    cerr << "Didn't understand time " << s;
+  }
 
   return spec;
 }
