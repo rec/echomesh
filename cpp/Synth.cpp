79c33671f13b20a355b1cfe1826276ca7627e20f
Tom Swirly <tom@swirly.com>
2012-09-26 23:42:01 -0400

    Now we read from the command line

--- cpp/Synth.cpp
+++ cpp/Synth.cpp
@@ -1,5 +1,44 @@
+#include <string.h>
+
 #include <iostream>
 
+using namespace std;
+
+namespace {
+
+const char PLAY[] = "play ";
+const char LIST[] = "list";
+
+void play(const string& s) {
+  cout << "Playing: " << s << endl;
+}
+
+void list() {
+  cout << "Listing" << endl;
+}
+
+void error(const string& s) {
+  cerr << "error: " << s << endl;
+}
+
+}
+
 int main(int argc, char **argv) {
-  std::cout << "Hello, world\n";
+  cout << "Synth starting" << endl;
+
+  string s;
+  while (getline(cin, s)) {
+    bool err = false;
+    if (!s.find(PLAY))
+      play(s.substr(strlen(PLAY)));
+    else if (!s.find(LIST))
+      list();
+    else
+      err = true;
+
+    if (err)
+      error(s);
+    else
+      cout << "ok" << endl;
+  }
 }
