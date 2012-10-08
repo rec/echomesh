337fff175f203b48d2de9a2d189e5428777ed59d
Tom Swirly <tom@swirly.com>
2012-10-08 14:51:33 -0400

    Clean up Twitter.py

--- python/Twitter.py
+++ python/Twitter.py
@@ -11,13 +11,3 @@ def getApi(config):
 
 def postUpdate(text, config):
   getApi(config).PostUpdate(Util.truncate(text, TWITTER_SIZE))
-
-  # twitter.Api(
-  #   consumer_key = auth['consumer_key'],
-  #   consumer_secret = auth['consumer_secret'],
-  #   access_token_key = auth['access_token_key'],
-  #   access_token_secret = auth['access_token_secret']
-  # ).PostUpdate(update)
-
-  # TODO: why didn't this work?
-  #   twitter.Api(*auth).PostUpdate(update)
