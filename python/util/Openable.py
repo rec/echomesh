3cb26b7b3d62dbff5e457ee6b3ed30283c8d0e62
Tom Swirly <tom@swirly.com>
2012-10-13 16:17:49 -0400

    Add missing files from Mac

---
+++ python/util/Openable.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+class Openable(object):
+  def __init__(self):
+    self.is_open = True
+
+  def close(self):
+    self.is_open = False
+
+  def __enter__(self):
+    return self
+
+  def __exit__(self, type, value, traceback):
+    self.close()
