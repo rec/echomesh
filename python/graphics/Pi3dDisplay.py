679269e82b0b0a5959634b3f4bcf99b7ddc699d8
Tom Ritchford <tom@swirly.com>
2012-10-22 20:08:38 +0000

    Tweak pi3d to work nicely inside a subdirectory.

--- python/graphics/Pi3dDisplay.py
+++ python/graphics/Pi3dDisplay.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from graphics.pi3d import pi3d
 
-class Py3dDisplay(object):
+from util import Openable
+
+class Pi3dDisplay(Openable.Openable):
   def __init__(self, config):
+    Openable.Openable.__init__(self)
     self.config = config
