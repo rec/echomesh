e24b9e8df90984a6e83c688356e6d22538511de9
Tom Ritchford <tom@swirly.com>
2012-10-23 19:00:47 +0000

    Tiny changes to Pi3dDisplay

--- python/graphics/Pi3dDisplay.py
+++ python/graphics/Pi3dDisplay.py
@@ -8,3 +8,9 @@ class Pi3dDisplay(Openable.Openable):
   def __init__(self, config):
     Openable.Openable.__init__(self)
     self.config = config
+    self.display = pi3d.display()
+    display.create2D(x, y, width, height)
+    display.setBackColor(0, 0, 0, 0)  # last is alpha
+
+  def run(self):
+    pass
