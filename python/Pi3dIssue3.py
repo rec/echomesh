868ee724713f7e72152b19f159266a43af80f4d5
Tom Ritchford <tom@swirly.com>
2012-10-31 01:48:50 +0000

    debugging

--- python/Pi3dIssue3.py
+++ python/Pi3dIssue3.py
@@ -2,27 +2,25 @@ from graphics.pi3d import pi3d
 
 import sys, random, array
 
-ENABLE_BUG = True
+ENABLE_BUG = not True
 
 scnx = 800
 scny = 600
 
-def init(disp):
-  disp.create2D(100,100,scnx,scny,0)
-  disp.setBackColour(1.0,0.2,0.6,1)
-  return disp
+def init_display():
+  DISPLAY.create2D(100,100,scnx,scny,0)
+  DISPLAY.setBackColour(1.0,0.2,0.6,1)
 
 DISPLAY = pi3d.display()
 if not ENABLE_BUG:
-  init(DISPLAY)
-
+  init_display()
 
 class Display(object):
   def __init__(self):
     self.texs = pi3d.textures()
     self.ball = self.texs.loadTexture("graphics/pi3d/textures/red_ball.png")
     if ENABLE_BUG:
-      init(DISPLAY)
+      init_display()
 
   def run(self):
     while True:
