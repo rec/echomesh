45aaa66a6436228ca8447c24f7788cd50d0ed1b8
Tom Ritchford <tom@swirly.com>
2012-10-31 01:46:03 +0000

    Simpler testcase for pi3d bug

--- python/Pi3dIssue3.py
+++ python/Pi3dIssue3.py
@@ -2,7 +2,7 @@ from graphics.pi3d import pi3d
 
 import sys, random, array
 
-ENABLE_BUG = not True
+ENABLE_BUG = True
 
 scnx = 800
 scny = 600
@@ -12,11 +12,8 @@ def init(disp):
   disp.setBackColour(1.0,0.2,0.6,1)
   return disp
 
-def init_display():
-  return init(pi3d.display())
-
+DISPLAY = pi3d.display()
 if not ENABLE_BUG:
-  DISPLAY = pi3d.display()
   init(DISPLAY)
 
 
@@ -25,15 +22,12 @@ class Display(object):
     self.texs = pi3d.textures()
     self.ball = self.texs.loadTexture("graphics/pi3d/textures/red_ball.png")
     if ENABLE_BUG:
-      self.display = pi3d.display()
-      init(self.display)
-    else:
-      self.display = DISPLAY
+      init(DISPLAY)
 
   def run(self):
     while True:
-      self.display.clear()
+      DISPLAY.clear()
       pi3d.sprite(self.ball, 200.0, 200.0, -2.0, 80.0, 80.0)
-      self.display.swapBuffers()
+      DISPLAY.swapBuffers()
 
 Display().run()
