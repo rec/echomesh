9d6296eb0237d80c0b8cdbae72be70017ebe498f
Tom Swirly <tom@swirly.com>
2013-01-08 13:13:13 -0500

    Newest version of pi3d

--- python/pi3d/shape/Model.py
+++ python/pi3d/shape/Model.py
@@ -10,34 +10,41 @@ from pi3d.util.RotateVec import rotate_vec
 from pi3d.util.Matrix import Matrix
 
 class Model(Shape):
-  def __init__(self, camera, light, fileString, name="", x=0.0, y=0.0, z=0.0,
+  def __init__(self, camera=None, light=None, file_string=None,
+               name="", x=0.0, y=0.0, z=0.0,
                rx=0.0, ry=0.0, rz=0.0, sx=1.0, sy=1.0, sz=1.0,
                cx=0.0, cy=0.0, cz=0.0):
     super(Model, self).__init__(camera, light, name, x, y, z, rx, ry, rz,
                                 sx, sy, sz, cx, cy, cz)
-    if("__clone__" in fileString): return #creating a copy but with pointer to buf
-    self.exf = fileString[-3:].lower()
+    if '__clone__' in file_string:
+      # Creating a copy but with pointer to buf.
+      return
+    self.exf = file_string[-3:].lower()
     if VERBOSE:
-      print "Loading ",fileString
+      print "Loading ",file_string
 
     if self.exf == 'egg':
-      self.model = loaderEgg.loadFileEGG(self, fileString)
+      self.model = loaderEgg.loadFileEGG(self, file_string)
       return self.model
     elif self.exf == 'obj':
-      self.model = loaderObj.loadFileOBJ(self, fileString)
+      self.model = loaderObj.loadFileOBJ(self, file_string)
       return self.model
     else:
       print self.exf, "file not supported"
       return None
-  
-  def clone(self, camera, light):
-    newModel = Model(camera, light, "__clone__." + self.exf)
+
+  def clone(self, camera = None, light = None):
+    newModel = Model(file_string = "__clone__",x=self.unif[0], y=self.unif[1], z=self.unif[2],
+               rx=self.unif[3], ry=self.unif[4], rz=self.unif[5], sx=self.unif[6], sy=self.unif[7], sz=self.unif[8],
+               cx=self.unif[9], cy=self.unif[10], cz=self.unif[11])
     newModel.buf = self.buf
     newModel.vGroup = self.vGroup
+    newModel.shader = self.shader
+    newModel.textures = self.textures
     return newModel
 
   def reparentTo(self, parent):
-    if not(self in parent.childModel):
+    if self not in parent.childModel:
       parent.childModel.append(self)
 
   def texSwap(self, texID, filename):
--- python/pi3d/shape/Shape.py
+++ python/pi3d/shape/Shape.py
@@ -4,18 +4,27 @@ from math import radians, pi, sin, cos
 
 from pi3d import *
 from pi3d.Buffer import Buffer
+from pi3d.context.Light import Light
 from pi3d.context.TextureLoader import TextureLoader
 from pi3d.util import Utility
 
 from pi3d.util.Loadable import Loadable
 
 class Shape(Loadable):
-  def __init__(self, camera, light, name, x, y, z,   rx, ry, rz,   sx, sy, sz,   cx, cy, cz):
+  def __init__(self, camera, light, name, x, y, z,
+               rx, ry, rz, sx, sy, sz, cx, cy, cz):
     super(Shape, self).__init__()
     self.name = name
+    light = light or Light.instance()
     # uniform variables all in one array!
-    self.unif = (c_float * 33)(x,y,z, rx,ry,rz, sx,sy,sz, cx,cy,cz, 0.5,0.5,0.5, 5000.0,0.8,0.0, 0.0,0.0,0.0, 0.0,0.0,0.0,
-      light.lightpos[0], light.lightpos[1], light.lightpos[2], 1.0,1.0,1.0, 0.1,0.1,0.1)
+    self.unif = (c_float * 33)(
+      x, y, z, rx, ry, rz,
+      sx, sy, sz, cx, cy, cz,
+      0.5, 0.5, 0.5, 5000.0, 0.8, 0.0,
+      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+      light.lightpos[0], light.lightpos[1], light.lightpos[2],
+      light.lightcol[0], light.lightcol[1], light.lightcol[2],
+      light.lightamb[0], light.lightamb[1], light.lightamb[2])
     """ in shader array of vec3 uniform variables:
     0  location 0-2
     1  rotation 3-5
@@ -25,25 +34,51 @@ class Shape(Loadable):
     5  fog distance and alph (only first two values used at moment) 15, 16
     6  camera position  18-20
     7  animation offets x, y, delta 21-23
-    8  light position, direction vector (if capacity for more lights is added these would go on the end of this list) 24-26
+    8  light position, direction vector (if capacity for more lights were added
+       these would go on the end of this list) 24-26
     9  light strength per shade 27-29
     10 light ambient values 30-32
     """
 
-    self.tr1 = array([[1.0, 0.0, 0.0, 0.0],[0.0, 1.0, 0.0, 0.0] ,[0.0, 0.0, 1.0, 0.0],[x-cx,y-cy,z-cz,1]])
+    self.tr1 = array([[1.0, 0.0, 0.0, 0.0],
+                      [0.0, 1.0, 0.0, 0.0],
+                      [0.0, 0.0, 1.0, 0.0],
+                      [x - cx, y - cy, z - cz,1]])
     s, c = sin(radians(self.unif[3])), cos(radians(self.unif[3]))
-    self.rox = array([[1.0, 0.0, 0.0, 0.0],[0.0, c, s, 0.0],[0.0, -s, c, 0.0],[0.0, 0.0, 0.0, 1.0]])
-    s, c = sin(radians(self.unif[4])), cos(radians(self.unif[4]))
-    self.roy = array([[c, 0.0, -s, 0.0],[0.0, 1.0, 0.0, 0.0],[s, 0.0, c, 0.0],[0.0, 0.0, 0.0, 1.0]])
-    s, c = sin(radians(self.unif[5])), cos(radians(self.unif[5]))
-    self.roz = array([[c, s, 0.0, 0.0],[-s, c, 0.0, 0.0],[0.0, 0.0, 1.0, 0.0],[0.0, 0.0, 0.0, 1.0]])
-    self.scl = array([[self.unif[6], 0.0, 0.0, 0.0],[0.0, self.unif[7], 0.0, 0.0],[0.0, 0.0, self.unif[8], 0.0],[0.0, 0.0, 0.0, 1.0]])
-    self.tr2 = array([[1.0,0.0,0.0,0.0], [0.0,1.0,0.0,0.0], [0.0,0.0,1.0,0.0], [self.unif[9], self.unif[10], self.unif[11], 1.0]])
-    self.MFlg = True
-    self.M = (c_float * 32)(0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0)
 
-    self.camera = camera
-    self.light = light
+    self.rox = array([[1.0, 0.0, 0.0, 0.0],
+                      [0.0, c, s, 0.0],
+                      [0.0, -s, c, 0.0],
+                      [0.0, 0.0, 0.0, 1.0]])
+    s, c = sin(radians(self.unif[4])), cos(radians(self.unif[4]))
+
+    self.roy = array([[c, 0.0, -s, 0.0],
+                      [0.0, 1.0, 0.0, 0.0],
+                      [s, 0.0, c, 0.0],
+                      [0.0, 0.0, 0.0, 1.0]])
+    s, c = sin(radians(self.unif[5])), cos(radians(self.unif[5]))
+
+    self.roz = array([[c, s, 0.0, 0.0],
+                      [-s, c, 0.0, 0.0],
+                      [0.0, 0.0, 1.0, 0.0],
+                      [0.0, 0.0, 0.0, 1.0]])
+
+    self.scl = array([[self.unif[6], 0.0, 0.0, 0.0],
+                      [0.0, self.unif[7], 0.0, 0.0],
+                      [0.0, 0.0, self.unif[8], 0.0],
+                      [0.0, 0.0, 0.0, 1.0]])
+
+    self.tr2 = array([[1.0, 0.0, 0.0, 0.0],
+                      [0.0, 1.0, 0.0, 0.0],
+                      [0.0, 0.0, 1.0, 0.0],
+                      [self.unif[9], self.unif[10], self.unif[11], 1.0]])
+    self.MFlg = True
+    self.M = (c_float * 32)(0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0)
+
+    self._camera = camera
     self.shader = None
     self.textures = []
 
@@ -52,67 +87,97 @@ class Shape(Loadable):
     # rendering with a different Shader/Texture. self.draw() relies on objects
     # inheriting from this filling buf with at least one element.
 
-  def draw(self, shdr=None, txtrs=None, ntl=None, shny=None):
-    # if called without parameters there has to have been a previous call to
-    # set_draw_details() for all the Buffers in buf[]
-    shader = shdr or self.shader
-    assert shader
+  def draw(self, shader=None, txtrs=None, ntl=None, shny=None, camera=None):
+    # If called without parameters, there has to have been a previous call to
+    # set_draw_details() for each Buffer in buf[].
+    from pi3d.Camera import Camera
+
+    camera = camera or self._camera or Camera.instance()
+    shader = shader or self.shader
     shader.use()
 
     if self.MFlg == True:
-      # calculate rotation and translation matrix for this model
-      # using numpy
+      # Calculate rotation and translation matrix for this model using numpy.
       self.MRaw = dot(self.tr2,
         dot(self.scl,
         dot(self.roy,
         dot(self.rox,
         dot(self.roz, self.tr1)))))
       self.M[0:16] = self.MRaw.ravel()
-      self.M[16:32] = dot(self.MRaw, self.camera.mtrx).ravel()
+      self.M[16:32] = dot(self.MRaw, camera.mtrx).ravel()
       self.MFlg = False
 
-    elif self.camera.movedFlag: #only do this is not done because model moved
-      self.M[16:32] = dot(self.MRaw, self.camera.mtrx).ravel()
+    elif camera.was_moved:
+      # Only do this if it's not done because model moved.
+      self.M[16:32] = dot(self.MRaw, camera.mtrx).ravel()
 
-    if self.camera.movedFlag:
-      self.unif[18:21] = self.camera.eye[0:3]
+    if camera.was_moved:
+      self.unif[18:21] = camera.eye[0:3]
 
     opengles.glUniformMatrix4fv(shader.unif_modelviewmatrix, 2, c_int(0),
                                 ctypes.byref(self.M))
-    # otherwise use existing version
+
     opengles.glUniform3fv(shader.unif_unif, 11, ctypes.byref(self.unif))
     for b in self.buf:
-      # Shape.draw has to be passed either parameter == None or values to pass on
-      b.draw(shdr, txtrs, ntl, shny)
+      # Shape.draw has to be passed either parameter == None or values to pass
+      # on.
+      b.draw(shader, txtrs, ntl, shny)
 
   def set_shader(self, shader):
     self.shader = shader
     for b in self.buf:
       b.shader = shader
 
-  def set_normal_shine(self, normtex, ntiles = 1.0, shinetex = None, shiny = 0.0, is_uv=True):
-    if is_uv: ofst = 0
-    else: ofst = -1
+  def set_normal_shine(self, normtex, ntiles=1.0, shinetex=None,
+                       shiny=0.0, is_uv=True):
+
+    ofst = 0 if is_uv else -1
     for b in self.buf:
-      if b.textures == None:
-        b.textures = []
-      if is_uv and len(b.textures) == 0:
+      b.textures = b.textures or []
+      if is_uv and not b.textures:
         b.textures = [normtex]
       while len(b.textures) < (2 + ofst):
         b.textures.append(None)
       b.textures[1 + ofst] = normtex
       b.unib[0] = ntiles
-      if shinetex != None:
+      if shinetex:
         while len(b.textures) < (3 + ofst):
           b.textures.append(None)
         b.textures[2 + ofst] = shinetex
         b.unib[1] = shiny
+
+  def set_draw_details(self, shader, textures, ntiles = 0.0, shiny = 0.0):
+    for b in self.buf:
+      b.set_draw_details(shader, textures, ntiles, shiny)
+
+  def set_material(self, mtrl):
+    for b in self.buf:
+      b.set_material(mtrl)
 
   def set_fog(self, fogshade, fogdist):
     # Set fog for this Shape, only it uses the shader smoothblend function from
     # 1/3 fogdist to fogdist.
     self.unif[12:15] = fogshade[0:3]
     self.unif[15] = fogdist
+    self.unif[16] = fogshade[3]
+    
+  def set_light(self, light, num = 0):
+    #TODO the unif array only has room for one light at the moment. Need MAXLIGHTS global variable
+    num = 0
+    #
+    stn = 24 + num * 9
+    self.unif[stn:(stn + 3)] = light.lightpos[0:3]
+    self.unif[(stn + 3):(stn + 6)] = light.lightcol[0:3]
+    self.unif[(stn + 6):(stn + 9)] = light.lightamb[0:3]
+    
+  def x(self):
+    return self.unif[0]
+
+  def y(self):
+    return self.unif[1]
+
+  def z(self):
+    return self.unif[2]
 
   def scale(self, sx, sy, sz):
     self.scl[0, 0] = sx
@@ -125,7 +190,7 @@ class Shape(Loadable):
     self.tr1[3, 0] = x - self.unif[9]
     self.tr1[3, 1] = y - self.unif[10]
     self.tr1[3, 2] = z - self.unif[11]
-    self.unif[0], self.unif[1], self.unif[2] = x, y, z
+    self.unif[0:3] = x, y, z
     self.MFlg = True
 
   def positionX(self, v):
@@ -229,70 +294,72 @@ class Shape(Loadable):
   def lathe(self, path, rise=0.0, loops=1.0, tris=True):
     s = len(path)
     rl = int(self.sides * loops)
-    ssize = rl * 6 * (s-1)
+    ssize = rl * 6 * (s - 1)
 
     pn = 0
     pp = 0
     tcx = 1.0 / self.sides
     pr = (pi / self.sides) * 2.0
     rdiv = rise / rl
-    ss=0
+    ss = 0
 
-    #find largest and smallest y of the path used for stretching the texture over
+    # Find largest and smallest y of the path used for stretching the texture
+    # over.
     miny = path[0][1]
     maxy = path[s-1][1]
-    for p in range (0, s):
-      if path[p][1] < miny: miny = path[p][1]
-      if path[p][1] > maxy: maxy = path[p][1]
+    for p in range(s):
+      if path[p][1] < miny:
+        miny = path[p][1]
+      if path[p][1] > maxy:
+        maxy = path[p][1]
 
-    verts=[]
-    norms=[]
-    idx=[]
-    tex_coords=[]
+    verts = []
+    norms = []
+    idx = []
+    tex_coords = []
 
-    opx=path[0][0]
-    opy=path[0][1]
+    opx = path[0][0]
+    opy = path[0][1]
 
-    for p in range (0, s):
+    for p in range(s):
 
-      px = path[p][0]*1.0
-      py = path[p][1]*1.0
+      px = path[p][0] * 1.0
+      py = path[p][1] * 1.0
 
-      tcy = 1.0 - ((py - miny)/(maxy - miny))
+      tcy = 1.0 - ((py - miny) / (maxy - miny))
 
-      #normalized 2D vector between path points TODO numpy?
+      # Normalized 2D vector between path points TODO numpy?
       dx, dy = Utility.vec_normal(Utility.vec_sub((px, py), (opx, opy)))
 
       for r in range (0, rl):
         sinr = sin(pr * r)
         cosr = cos(pr * r)
-        verts.append((px * sinr,py,px * cosr))
-        norms.append((-sinr*dy,dx,-cosr*dy))
-        tex_coords.append((1.0-tcx * r, tcy))
+        verts.append((px * sinr, py, px * cosr))
+        norms.append((-sinr * dy, dx, -cosr * dy))
+        tex_coords.append((1.0 - tcx * r, tcy))
         py += rdiv
-      #last path profile (tidies texture coords)
-      verts.append((0,py,px))
-      norms.append((0,dx,-dy))
-      tex_coords.append((0,tcy))
 
-      if p < s-1:
-        pn += (rl+1)
-        for r in range (0, rl):
-          idx.append((pp+r+1,pp+r,pn+r))
-          idx.append((pn+r,pn+r+1,pp+r+1))
-        pp += (rl+1)
+      # Last path profile (tidies texture coords).
+      verts.append((0, py, px))
+      norms.append((0, dx, -dy))
+      tex_coords.append((0, tcy))
 
-      opx=px
-      opy=py
+      if p < s - 1:
+        pn += (rl + 1)
+        for r in range(rl):
+          idx.append((pp + r + 1, pp + r, pn + r))
+          idx.append((pn + r, pn + r + 1, pp + r + 1))
+        pp += (rl + 1)
 
-      #print verts,norms,idx
-    buf = Buffer(self, verts, tex_coords, idx, norms)
-    return buf
+      opx = px
+      opy = py
+
+    return Buffer(self, verts, tex_coords, idx, norms)
 
 
 def normalize_vector(begin, end):
   diff = [e - b for b, e in zip(begin, end)]
   mag = Utility.magnitude(*diff)
-  mult = 1 / mag if mag > 0.0 else 0.0
+  mult = (1 / mag) if mag > 0.0 else 0.0
   return [x * mult for x in diff]
 
