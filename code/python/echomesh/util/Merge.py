7c099e0b1f2818691214320e86e2e6c48ff8814c
Tom Swirly <tom@swirly.com>
2013-01-14 19:09:08 -0500

    Consolidated merge and difference

--- code/python/echomesh/util/Merge.py
+++ code/python/echomesh/util/Merge.py
@@ -2,26 +2,32 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import copy
 
-def merge(old, new, path=[]):
+def merge_or_diff(is_merge, old, new, path=''):
   """Merges two dictionaries, mutating the dictionary "old"."""
   nothing = ()
   for key, new_v in new.iteritems():
-    new_path = path + [key]
+    new_path = '%s:%s' % (path, key)
     old_v = old.get(key, nothing)
 
     if old_v is nothing:
-      raise Exception('Tried to override non-existent key ' +
-                      ':'.join(new_path))
+      if is_merge:
+        raise Exception('Tried to override non-existent key ' +
+                        ':'.join(new_path))
+      else:
+        continue
 
     if isinstance(old_v, dict):
       if isinstance(new_v, dict):
-        merge(old_v, new_v, new_path)
+        merge_or_diff(is_merge, old_v, new_v, new_path)
       else:
         raise Exception('Tried to override dict with non-dict for key ' +
                         ':'.join(new_path))
 
-    elif old_v is nothing or not isinstance(new_v, dict):
-      old[key] = new_v
+    elif not isinstance(new_v, dict):
+      if is_merge:
+        old[key] = new_v
+      else:
+        del old[key]
 
     else:
       raise Exception('Tried to override non-dict with dict for key ' +
@@ -29,36 +35,11 @@ def merge(old, new, path=[]):
 
   return old
 
-def merge_all(target, *others):
-  return reduce(merge, others, target)
+def merge(old, new):
+  return merge_or_diff(True, old, new)
 
 def difference(old, new):
-  """
-  Returns the elements that are different between two dictionaries, recursively.
-  """
-  nothing = ()
-  for key, new_v in new.iteritems():
-    old_v = old.get(key, nothing)
-
-    if old_v is nothing:
-      continue
-
-    if isinstance(old_v, dict):
-      if isinstance(new_v, dict):
-        difference(old_v, new_v)
-        if not old_v:
-          del old[key]
-      else:
-        raise Exception('Tried to override dict with non-dict for key ' + key)
-
-    elif not isinstance(new_v, dict):
-      del old[key]
-
-    else:
-      raise Exception('Tried to override non-dict with dict for key ' + key)
-
-  return old
-
+  return merge_or_diff(False, old, new)
 
 """
 >>> Merge.merge({1:2, 3:5}, {1:4, 2:7})
