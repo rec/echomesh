5b70d8b601d0223fa425d87ebb0df1110710152c
Tom Swirly <tom@swirly.com>
2013-01-15 18:07:57 -0500

    Tiny cleanup of Echomesh

--- code/python/Echomesh.py
+++ code/python/Echomesh.py
@@ -1,12 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import contextlib
 import os
-import os.path
 import sys
-import threading
-import time
-import traceback
 
 from echomesh.command import Score
 from echomesh.config import Config
