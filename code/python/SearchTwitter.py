81e8792d24c53dcee8e1652ded6a9f740d2de859
Tom Swirly <tom@swirly.com>
2013-01-16 00:20:27 -0500

    Improved SearchTwitter.py functionality

--- code/python/SearchTwitter.py
+++ code/python/SearchTwitter.py
@@ -2,18 +2,19 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from echomesh.network import SearchTwitter
+from echomesh.util.thread import Closer
 
 if __name__ == '__main__':
-  import sys
-
-  try:
-    query = sys.argv[1]
-  except:
-    query = '#aaronswartz'
-
-  loop = SearchTwitter.Loop(query, print)
-  loop.start()
-  raw_input('Press return to exit\n')
-  loop.close()
-  loop.join()
+  with Closer.Closer() as closer:
+    tags = []
+    while closer.is_open:
+      tag = raw_input('Enter a tag or quit to exit\n').strip()
+      if tag == 'quit':
+        closer.close()
+      else:
+        tags.append(tag)
+        print('Now following', *tags)
+        loop = SearchTwitter.Loop(tag, print)
+        loop.start()
+        closer.add_openable(loop)
 
