16708fadadbbe4fdf03d7da5b750eaa63bef991d
Tom Swirly <tom@swirly.com>
2013-01-25 15:59:29 -0500

    Oops, forgot new util.Registry

---
+++ code/python/echomesh/util/Registry.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+class Registry(object):
+  def __init__(self, case_insensitive=True):
+    self.registry = {}
+    self.case_insensitive = case_insensitive
+
+  def register(self, item, name=None):
+    name = name or item.__name__
+    if self.case_insensitive:
+      name = name.lower()
+    old_item = self.registry.get(name, None)
+    if old_item is not item:
+      if old_item:
+        raise Exception('Conflicting registrations for %s' % name)
+      self.registry[name] = item
+
+  def get(self, name, default=None):
+    return self.registry.get(name, default)
