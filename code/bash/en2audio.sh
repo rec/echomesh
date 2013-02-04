a2e5af69c31f365efa6e4d58eb32447a9726785b
Tom Swirly <tom@swirly.com>
2013-02-04 14:17:56 -0500

    Changes to UniqueName, en2audio

--- code/bash/en2audio.sh
+++ code/bash/en2audio.sh
@@ -4,3 +4,5 @@
 # from:  http://stackoverflow.com/questions/9163988/download-mp3-from-google-translate-text-to-speech
 # see also: http://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
 wget -q -U Mozilla -O "$*.mp3" "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=$*"
+
+curl --output foo.mp3 --user-agent Mozilla "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=who+am+I"
--- code/python/echomesh/util/UniqueName.py
+++ code/python/echomesh/util/UniqueName.py
@@ -4,8 +4,8 @@ import re
 
 MATCH_NAME = re.compile(r'(.*)\.(\d+)$')
 
-def unique_name(name, elements):
-  while name in elements:
+def unique_name_match(name, matcher):
+  while matcher(name):
     match = MATCH_NAME.match(name)
     if match:
       base, suffix = match.groups()
@@ -15,4 +15,12 @@ def unique_name(name, elements):
     name = '%s.%d' % (base, suffix)
 
   return name
+
+def unique_name(name, elements):
+  return unique_name_match(name, elements.__contains__)
+
+def unique_file_name(fname, suffix):
+  def matcher(name):
+    return os.path.exists('%s.%s' % (name, suffix))
+  return '%s.%s' % (unique_name_match(fname, matcher), suffix)
 
