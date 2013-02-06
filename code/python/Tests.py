83ea11325db27c66c892b233ed18669b5ea1ad3b
Tom Swirly <tom@swirly.com>
2013-02-06 04:13:16 -0500

    New LEDBank.py, split out GammaTable.py and WheelColor.py

--- code/python/Tests.py
+++ code/python/Tests.py
@@ -334,7 +334,7 @@ u'failed eval: invalid identifier "foo"'
 # >>> test_parse('0xff')  # Doesn't yet work.
 # 255
 
->>> [Color.wheel_color(r / 10.0) for r in range(11)]
+>>> [WheelColor.wheel_color(r / 10.0) for r in range(11)]
 [array([ 0.,  1.,  0.]), array([ 0.3,  0.7,  0. ]), array([ 0.6,  0.4,  0. ]), array([ 0.9,  0.1,  0. ]), array([ 0. ,  0.2,  0.8]), array([ 0. ,  0.5,  0.5]), array([ 0. ,  0.8,  0.2]), array([ 0.9,  0. ,  0.1]), array([ 0.6,  0. ,  0.4]), array([ 0.3,  0. ,  0.7]), array([ 0.,  1.,  0.])]
 
 """
@@ -343,12 +343,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from echomesh.base.CommandFile import resolve_scope
 from echomesh.base import Merge
+from echomesh.graphics import WheelColor
 from echomesh.util.UniqueName import unique_name
 from echomesh.util.math import Average
 from echomesh.util.math import Envelope
 from echomesh.util.math import Units
 from echomesh.util.math.WeightedRandom import WeightedRandom
-from echomesh.util import Color
 
 from pyparsing import Expressions
 from pyparsing.pyparsing import ParseException
---
+++ code/python/echomesh/graphics/LEDBank.py
@@ -0,0 +1,52 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy
+
+from echomesh.graphics import GammaTable
+
+class ChannelOrder(object):
+
+class LEDBank(object):
+  RGB_ORDER = lambda r, g, b: (r, g, b)
+  GRB_ORDER = lambda r, g, b: (g, r, b)
+  BRG_ORDER = lambda r, g, b: (b, r, g)
+
+  def __init__(self, count, device='/dev/spidev0.0',
+               correct = GammaTable.correct,
+               channel_order = LEDBank.RGB_ORDER):
+    self.count = count
+    self.channel_order = channel_order
+    self.correct = correct
+    self.buffer = bytearray(3 * count + 4)
+    self.buffer_dirty = False
+    self.pixels = numpy.array([0.0] * (3 * count))
+    self.pixels_dirty = False
+    self.device = device
+    self.spi = open(device, 'wb')
+    self.brightness = 1.0
+
+  def set_brightness(self, brightness):
+    self.brightness = brightness
+    self.pixels_dirty = True
+
+  def update(self):
+    if self.pixels_dirty:
+      for i in range(self.count):
+        self._set_buffer_entry(i)
+      self.buffer_dirty = True
+      self.pixels_dirty = False
+
+    if self.buffer_dirty:
+      self.spi.write(self.buffer)
+      self.buffer_dirty = False
+
+  def set_pixel(self, i, rgb):
+    self.pixels[3 * i:3 * (i + 1)] = rgb
+    self._set_buffer_entry(i)
+    self.buffer_dirty = True
+
+  def _set_buffer_entry(self, i):
+    b, e = 3 * i, 3 * (i + 1)
+    rgb = (self.correct(self.pixels[i] * self.brightness) for i in range(b, e))
+    self.buffer[b:e] = self.channel_order(*rgb)
+
