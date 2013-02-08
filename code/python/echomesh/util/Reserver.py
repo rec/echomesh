c85f4e490a58fb66f7a87b77cb921c1cc2ccde1d
Tom Swirly <tom@swirly.com>
2013-02-08 00:45:37 -0500

    Tests for Reserver

--- code/python/echomesh/util/Reserver.py
+++ code/python/echomesh/util/Reserver.py
@@ -16,37 +16,43 @@ class Reserver(object):
   reserve it.
   """
   def __init__(self, lock=None):
-    self.reserved = collections.defaultdict(lambda: 0)
-    self.reserved_uniquely = set()
+    self._reserved = collections.defaultdict(lambda: 0)
+    self._reserved_uniquely = set()
     self.lock = lock or threading.Lock()
 
-  def reserve(self, items):
+  def reserved(self):
     with self.lock:
-      res = self.reserved_uniquely.intersection(items)
+      return self._reserved.keys()
+
+  def reserve(self, *items):
+    with self.lock:
+      res = self._reserved_uniquely.intersection(items)
       if res:
-        raise Exception('%s are already uniquely reserved.' % ', '.join(res))
+        print(res)
+        raise Exception('%s are already uniquely reserved.' % res)
       for item in items:
-        self.reserved[item] += 1
+        self._reserved[item] += 1
 
   def reserve_uniquely(self, *items):
     with self.lock:
-      res = [i for i in items if self.reserved[i]]
+      res = [i for i in items if self._reserved[i]]
       if res:
-        raise Exception('%s are already reserved.' % ', '.join(res))
+        raise Exception('%s are already reserved.' % res)
       for item in items:
-        self.reserved[item] += 1
-      self.reserved_uniquely.update(items)
+        self._reserved[item] += 1
+      self._reserved_uniquely.update(items)
 
-  def release(self, *items):
+  def unreserve(self, *items):
     with self.lock:
       not_reserved = []
       for item in items:
-        count = self.reserved[item]
+        count = self._reserved[item]
         if count <= 1:
           if count < 1:
             not_reserved.append(item)
-          del self.reserved[item]
+          del self._reserved[item]
         else:
-          self.reserved[item] = count - 1
-
-      self.reserved_uniquely.difference_update(items)
+          self._reserved[item] = count - 1
+      self._reserved_uniquely.difference_update(items)
+    if not_reserved:
+      LOGGER.warning("Some items were not reserved: %s", not_reserved)
--- code/python/echomesh/util/Tests.py
+++ code/python/echomesh/util/Tests.py
@@ -1,5 +1,52 @@
 """
 
+>>> res = Reserver()
+
+>>> res.reserved()
+[]
+
+>>> res.reserve(1, 2, 3)
+>>> set(res.reserved())
+set([1, 2, 3])
+
+>>> res.reserve(1, 2, 3)
+>>> set(res.reserved())
+set([1, 2, 3])
+
+>>> res.unreserve(1, 2)
+>>> set(res.reserved())
+set([1, 2, 3])
+
+>>> res.unreserve(1)
+>>> set(res.reserved())
+set([2, 3])
+
+>>> res.unreserve(2, 3)
+>>> set(res.reserved())
+set([3])
+
+>>> res.unreserve(3)
+>>> set(res.reserved())
+set([])
+
+>>> res.reserve_uniquely(1, 2)
+>>> set(res.reserved())
+set([1, 2])
+
+>>> res.reserve_uniquely(1, 2)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "echomesh/util/Reserver.py", line 41, in reserve_uniquely
+    raise Exception('%s are already reserved.' % res)
+Exception: [1, 2] are already reserved.
+
+>>> res.reserve(1, 2)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "echomesh/util/Reserver.py", line 32, in reserve
+    raise Exception('%s are already uniquely reserved.' % res)
+Exception: set([1, 2]) are already uniquely reserved.
+
 >>> unique_name('', [])
 u''
 
@@ -19,4 +66,5 @@ u'bar.2'
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from echomesh.util.Reserver import Reserver
 from echomesh.util.UniqueName import unique_name
