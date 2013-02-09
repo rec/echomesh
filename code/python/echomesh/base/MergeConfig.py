3425f2665e600f4275687cd576450044c3a1e9b1
Tom Swirly <tom@swirly.com>
2013-02-09 14:29:05 -0500

    Better messages from MergeConfig

--- code/python/echomesh/base/MergeConfig.py
+++ code/python/echomesh/base/MergeConfig.py
@@ -14,7 +14,7 @@ def _merge_level_files():
     try:
       cfg = File.yaml_load(f)
     except:
-      raise Exception('Error in configuration file %s' % f)
+      _add_exception_suffix(e, ' loading configuration file %s' % f)
 
     if config is None:
       assert cfg, "Unabled to read default config file %s" % f
@@ -23,22 +23,27 @@ def _merge_level_files():
       try:
         Merge.merge(config, cfg)
       except Exception as e:
-        _add_exception_suffix(e, ' in configuration file %s' % f)
+        _add_exception_suffix(e, ' merging configuration file %s' % f)
         raise
   return config
 
 def _merge_command_line_arguments(args, config):
+  # Merge in command line arguments.
   for i, arg in enumerate(args):
-    try:
-      cfgs = File.yaml_load_stream(arg)
-    except:
-      raise Exception('Error in command line argument %d: "%s"' % (i, arg))
+    if arg and arg[0] == '{':
+      try:
+        cfgs = File.yaml_load_stream(arg)
+      except:
+        _add_exception_suffix(e, ' parsing command line argument %d: "%s"' %
+                              (i, arg))
+        raise
 
-    try:
-      Merge.merge_all(config, *cfgs)
-    except Exception as e:
-      _add_exception_suffix(e, ' in command line argument %d: "%s"' % (i, arg))
-      raise
+      try:
+        Merge.merge_all(config, *cfgs)
+      except Exception as e:
+        _add_exception_suffix(e, ' merging command line argument %d: "%s"' %
+                              (i, arg))
+        raise
   return config
 
 def merge(args):
