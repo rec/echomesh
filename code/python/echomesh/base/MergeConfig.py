041b3351fabba409ea70452746920d0c9d108051
Tom Swirly <tom@swirly.com>
2013-02-10 18:31:39 -0500

    Simplify MergeConfig

--- code/python/echomesh/base/MergeConfig.py
+++ code/python/echomesh/base/MergeConfig.py
@@ -1,50 +1,39 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import itertools
+
 from echomesh.base import CommandFile
 from echomesh.base import Yaml
 from echomesh.base import Merge
 
-def _add_exception_suffix(e, suffix):
+def _add_exception_suffix(e, *suffixes):
+  suffix = ' '.join(suffixes)
   e.args = tuple(a + suffix for a in e.args)
 
-def _merge_level_files():
-  # Merge configuration files from the command directory hierarchy.
-  config = None
-  for f in reversed(CommandFile.expand('config.yml')):
+def _parse(items, parser, message):
+  for item in items:
     try:
-      cfg = Yaml.read_one(f)
+      for cfg in parser(item):
+        if cfg:
+          yield cfg, item, message
     except Exception as e:
-      _add_exception_suffix(e, ' loading configuration file %s' % f)
+      _add_exception_suffix(e, message, 'parsing', item)
       raise
 
+def merge(args):
+  files = reversed(CommandFile.expand('config.yml'))
+  file_iter = _parse(files, Yaml.read_all, 'config file')
+  args = (a for a in args if a[0] in '{[')
+  arg_iter = _parse(args, Yaml.decode_all, 'command line argument')
+  config = None
+
+  for cfg, item, message in itertools.chain(file_iter, arg_iter):
     if config is None:
-      assert cfg, "Unable to read default config file %s" % f
+      assert cfg, "Unable to read default %s %s" % (message, item)
       config = cfg
     else:
       try:
         Merge.merge(config, cfg)
       except Exception as e:
-        _add_exception_suffix(e, ' merging configuration file %s' % f)
-        raise
+        _add_exception_suffix(e, message, 'merging', item)
   return config
-
-def _merge_command_line_arguments(args, config):
-  # Merge in command line arguments.
-  for i, arg in enumerate(args):
-    if arg.startswith('{'):
-      try:
-        cfgs = Yaml.decode_all(arg)
-      except Exception as e:
-        print('Error %s parsing command line argument %d: "%s"' %
-              (str(e), i, arg))
-
-      try:
-        Merge.merge_all(config, *cfgs)
-      except Exception as e:
-        print('Error %s merging command line argument %d: "%s"' %
-              (str(e), i, arg))
-
-  return config
-
-def merge(args):
-  return _merge_command_line_arguments(args, _merge_level_files())
