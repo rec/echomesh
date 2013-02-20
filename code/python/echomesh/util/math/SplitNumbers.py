6349ac9c8d58d74577da5bcf9ccc3ad68ef86a62
Tom Swirly <tom@swirly.com>
2013-02-20 14:21:34 -0500

    Added missing SplitNumbers.py

---
+++ code/python/echomesh/util/math/SplitNumbers.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import six
+
+from echomesh.util.math import Units
+
+def split(items):
+  kwds = {}
+  numeric = []
+  for k, v in items.iteritems():
+    if isinstance(k, six.string_types) and k[0].isalpha():
+      kwds[k] = v
+    else:
+      numeric.append([Units.convert(k), Units.convert(v)])
+  return kwds, sorted(numeric)
+
+
