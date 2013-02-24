c8863c4ba56295258556cb8d20cc08adecf2ca0f
Tom Swirly <tom@swirly.com>
2013-02-24 02:48:35 -0500

    Remote score command worked right first time\!

--- code/python/echomesh/command/Broadcast.py
+++ code/python/echomesh/command/Broadcast.py
@@ -1,22 +1,26 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from echomesh.command import Register
+from echomesh.command import Show
 from echomesh.util import Log
 
 LOGGER = Log.logger(__name__)
 
-def broadcast(echomesh_instance, on_or_off):
-  on_or_off = on_or_off.lower()
-  b_on = on_or_off in ['on', 'true']
-  if not (b_on or on_or_off in ['off', 'false']):
-    raise Exception('You can only turn broadcast mode "on" or "off".')
-  name = 'ON' if b_on else 'off'
-  if b_on == echomesh_instance.broadcasting():
-    message = 'was already'
+def broadcast(echomesh_instance, on_or_off=None):
+  if on_or_off is None:
+    Show.broadcast(echomesh_instance)
   else:
-    echomesh_instance.set_broadcasting(b_on)
-    message = 'is now'
-  LOGGER.print('broadcast mode %s %s.', message, name)
+    on_or_off = on_or_off.lower()
+    b_on = on_or_off in ['on', 'true']
+    if not (b_on or on_or_off in ['off', 'false']):
+      raise Exception('You can only turn broadcast mode "on" or "off".')
+    name = 'ON' if b_on else 'off'
+    if b_on == echomesh_instance.broadcasting():
+      message = 'was already'
+    else:
+      echomesh_instance.set_broadcasting(b_on)
+      message = 'is now'
+    LOGGER.print('broadcast mode %s %s.', message, name)
 
 BROADCAST_HELP = """
 Set the broadcast mode on or off.
