0ef5f1354a8652d9ae89f70cf854df2ff0ac6c7c
Tom Ritchford <tom@swirly.com>
2013-02-25 18:25:28 -0500

    Cleaned up test code regarding image sprites

--- code/python/echomesh/graphics/ImageSprite.py
+++ code/python/echomesh/graphics/ImageSprite.py
@@ -20,14 +20,13 @@ DEFAULT_Z = 100.0
 
 IMAGE_DIRECTORY = DefaultFile.DefaultFile('asset/image')
 
-CREATE_SPRITE_IN_CONSTRUCTOR = not True
-
 class DeferredSprite(Loadable):
-  def __init__(self, repaint, imagename, coords):
+  def __init__(self, repaint, imagename, coords, shader):
     super(DeferredSprite, self).__init__()
     self.repaint = repaint
     self.imagename = imagename
     self.coords = coords
+    self.shader = shader
 
   def _load_opengl(self):
     if not ImageSprite.CACHE:
@@ -36,8 +35,7 @@ class DeferredSprite(Loadable):
     x, y, z = self.coords
     self.pi3d_sprite = pi3d.ImageSprite(texture,
                                         w=texture.ix, h=texture.iy,
-                                        shader=Shader.DEFAULT_SHADER,
-                                        # shader=Shader.shader(shader),
+                                        shader=Shader.shader(self.shader),
                                         x=x, y=y, z=z)
 
 class ImageSprite(Runnable):
@@ -53,7 +51,6 @@ class ImageSprite(Runnable):
       LOGGER.error('Unknown keyword%s: %s', s, ', '.join(kwds))
 
     self.imagename = IMAGE_DIRECTORY.expand(file)
-    LOGGER.debug('Opening sprite %s', self.imagename)
 
     self._loops = loops
     self._loop_number = 0
@@ -62,7 +59,6 @@ class ImageSprite(Runnable):
     self._size = Envelope(size)
     self._z = Envelope(z)
 
-    assert not shader, "We can't create shaders except on the main thread."
     self._time = 0
     if duration:
       self._duration = Units.convert(duration)
@@ -72,29 +68,8 @@ class ImageSprite(Runnable):
 
     if not self._duration:
       LOGGER.warning('An image sprite had a zero duration.')
-    self._sprite = None
-    self.sprite_added = False
-    if CREATE_SPRITE_IN_CONSTRUCTOR:
-      self.sprite()
-    else:
-      self.deferred_sprite = DeferredSprite(self.repaint, self.imagename,
-                                            self.coords(0))
-
-  def sprite(self):
-    if not self._sprite:
-      print('CREATING sprite')
-      x, y, z = self.coords(0)
-      if not ImageSprite.CACHE:
-        ImageSprite.CACHE = pi3d.TextureCache()
-      texture = ImageSprite.CACHE.create(self.imagename, defer=True)
-      self._sprite = pi3d.ImageSprite(texture,
-                                          w=texture.ix, h=texture.iy,
-                                          shader=Shader.DEFAULT_SHADER,
-                                          # shader=Shader.shader(shader),
-                                          x=x, y=y, z=z)
-      setattr(self._sprite, 'repaint', self.repaint)
-      self._add_sprite()
-    return self._sprite
+    self.deferred_sprite = DeferredSprite(self.repaint, self.imagename,
+                                          self.coords(0), shader)
 
   def coords(self, t):
     x, y = self._position.interpolate(t)
@@ -115,10 +90,7 @@ class ImageSprite(Runnable):
         self.stop()
         return
 
-    if CREATE_SPRITE_IN_CONSTRUCTOR:
-      sprite = self.sprite()
-    else:
-      sprite = self.deferred_sprite.pi3d_sprite
+    sprite = self.deferred_sprite.pi3d_sprite
 
     sprite.position(*self.coords(elapsed))
     size = self._size.interpolate(elapsed)
@@ -133,23 +105,10 @@ class ImageSprite(Runnable):
     self._add_sprite()
 
   def _add_sprite(self):
-    if CREATE_SPRITE_IN_CONSTRUCTOR:
-      if self.is_running and self._sprite and not self.sprite_added:
-        self.sprite_added = True
-        pi3d.Display.Display.INSTANCE.add_sprites(self._sprite)
-        print('_add_sprite success')
-      else:
-        print('_add_sprite FAIL', self.is_running, self._sprite,
-              self.sprite_added)
-    else:
-      pi3d.Display.Display.INSTANCE.add_sprites(self.deferred_sprite)
+    pi3d.Display.Display.INSTANCE.add_sprites(self.deferred_sprite)
 
 
   def _on_stop(self):
-    if CREATE_SPRITE_IN_CONSTRUCTOR:
-       if self.sprite_added and self._sprite:
-         pi3d.Display.Display.INSTANCE.remove_sprites(self._sprite)
-    else:
-      pi3d.Display.Display.INSTANCE.remove_sprites(self.deferred_sprite)
+    pi3d.Display.Display.INSTANCE.remove_sprites(self.deferred_sprite)
 
 
