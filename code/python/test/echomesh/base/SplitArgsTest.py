970ff900173ed6f89306842a9b5b1cfeb924d0c2
Tom Ritchford <tom@swirly.com>
2013-03-04 14:20:44 -0500

    Split off SplitArgsTest.py

--- code/python/test/echomesh/base/SplitArgsTest.py
+++ code/python/test/echomesh/base/SplitArgsTest.py
@@ -1,40 +1,4 @@
 """
->>> split_words(', hello, there!')
-[u'hello', u'there!']
-
->>> split_words('')
-[]
-
->>> split_words('one')
-[u'one']
-
->>> split_words('one two')
-[u'one', u'two']
-
->>> split_words(' one two')
-[u'one', u'two']
-
->>> split_words('one two')
-[u'one', u'two']
-
->>> split_words(' one two ')
-[u'one', u'two']
-
->>> pair_split([])
-[]
-
->>> pair_split(split_words('hello'))
-[(u'hello', None)]
-
->>> pair_split(split_words('hello as hell'))
-[(u'hello', u'hell')]
-
->>> pair_split(split_words('hello as hell, dogs as cats'))
-[(u'hello', u'hell'), (u'dogs', u'cats')]
-
->>> pair_split(split_words('hello,  hell as dogs, cats'))
-[(u'hello', u'dogs'), (u'hell', u'cats')]
-
 >>> list(split_args([]))
 []
 
@@ -82,4 +46,4 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from echomesh.base.Split import *
+from echomesh.base.SplitArgs import *
---
+++ code/python/test/echomesh/util/SplitTests.py
@@ -0,0 +1,42 @@
+"""
+>>> split_words(', hello, there!')
+[u'hello', u'there!']
+
+>>> split_words('')
+[]
+
+>>> split_words('one')
+[u'one']
+
+>>> split_words('one two')
+[u'one', u'two']
+
+>>> split_words(' one two')
+[u'one', u'two']
+
+>>> split_words('one two')
+[u'one', u'two']
+
+>>> split_words(' one two ')
+[u'one', u'two']
+
+>>> pair_split([])
+[]
+
+>>> pair_split(split_words('hello'))
+[(u'hello', None)]
+
+>>> pair_split(split_words('hello as hell'))
+[(u'hello', u'hell')]
+
+>>> pair_split(split_words('hello as hell, dogs as cats'))
+[(u'hello', u'hell'), (u'dogs', u'cats')]
+
+>>> pair_split(split_words('hello,  hell as dogs, cats'))
+[(u'hello', u'dogs'), (u'hell', u'cats')]
+
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from echomesh.util.Split import *
