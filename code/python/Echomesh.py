2b60b1f2df1121a4b40ec06351777931c272f08b
Tom Ritchford <tom@swirly.com>
2013-03-04 16:20:14 -0500

    Add missing files

---
+++ code/python/Echomesh.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+if __name__ == '__main__':
+  try:
+    from echomesh import Main
+    Main.run()
+  except:
+    import traceback
+    print(traceback.format_exc())
