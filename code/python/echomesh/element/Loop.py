d6a4843c0c7902e9418e31fc1d122c1c439ab163
Tom Ritchford <tom@swirly.com>
2013-03-12 15:17:56 -0400

    Keep bringing things into Loop

--- code/python/echomesh/element/Loop.py
+++ code/python/echomesh/element/Loop.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import threading
 import time
 
 from echomesh.element import Element
 from echomesh.util import Log
 from echomesh.util.math.Units import INFINITY
-from echomesh.util.thread import ThreadLoop
+from echomesh.util.thread.MasterRunnable import MasterRunnable
 
 LOGGER = Log.logger(__name__)
 
@@ -13,7 +14,52 @@ LOGGER = Log.logger(__name__)
 DEFAULT_TIMEOUT = 1.0
 DEFAULT_INTERVAL = INFINITY
 
-class TimeLoop(ThreadLoop.ThreadLoop):
+class ThreadRunnable(MasterRunnable):
+  def __init__(self, target=None, name=None, report_error=False):
+    super(ThreadRunnable, self).__init__()
+    self.name = name or repr(self)
+    if target:
+      self.target = target
+    else:
+      assert self.target
+    self.report_error = report_error
+
+  def _on_run(self):
+    def target():
+      try:
+        self.target()
+      except Exception as e:
+        if self.is_running or self.report_error:
+          LOGGER.error('Thread %s reports an error:', self.name, exc_info=1)
+        self.pause()
+      self._after_thread_pause()
+
+    super(ThreadRunnable, self)._on_run()
+    self._before_thread_start()
+    self.thread = threading.Thread(target=target)
+    self.thread.daemon = True
+    self.thread.start()
+
+  def _before_thread_start(self):
+    pass
+
+  def _after_thread_pause(self):
+    pass
+
+
+
+class ThreadLoop(ThreadRunnable):
+  def __init__(self, single_loop=None, name=None, report_error=False):
+    super(ThreadLoop, self).__init__(report_error=report_error)
+    self.name = name or repr(self)
+    self.single_loop = single_loop or self.single_loop
+    assert self.single_loop
+
+  def target(self):
+    while self.is_running:
+      self.single_loop()
+
+class TimeLoop(ThreadLoop):
   def __init__(self,
                interval=DEFAULT_INTERVAL,
                loop_target=None,
@@ -62,9 +108,6 @@ class TimeLoop(ThreadLoop.ThreadLoop):
       else:
         LOGGER.error('Sleeping for negative time %s', sleep_time, limit=10)
 
-  def next_time(self, t):
-    return t + self.interval
-
   def _after_thread_pause(self):
     self.pause_time = self.timer() - self.start_time
     print('!!!! 2', self.pause_time)
@@ -81,6 +124,12 @@ class Loop(Element.Element):
 
     self.add_slave(self.time_loop)
     self.time_loop.add_pause_only_slave(self)
+
+  def next_time(self, t):
+    return t + self.time_loop.interval
+
+  def loop_target(self, t):
+    pass
 
   # You can implement Loop.next_time and Loop.loop_target to override the
   # methods in TimeLoop.
