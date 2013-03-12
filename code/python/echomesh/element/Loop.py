f8947f20abea6f0cd2ee381c83fcab7ce9ba7031
Tom Ritchford <tom@swirly.com>
2013-03-12 15:51:47 -0400

    Loop's threading is entirely self-contained now.

--- code/python/echomesh/element/Loop.py
+++ code/python/echomesh/element/Loop.py
@@ -14,24 +14,22 @@ LOGGER = Log.logger(__name__)
 DEFAULT_TIMEOUT = 1.0
 DEFAULT_INTERVAL = INFINITY
 
-class TimeLoop(MasterRunnable):
-  def __init__(self,
-               interval=DEFAULT_INTERVAL,
-               loop_target=None,
-               name='TimeLoop',
-               next_time=None,
-               report_error_on_close=False,
-               timeout=DEFAULT_TIMEOUT):
-    super(TimeLoop, self).__init__()
+class Loop(Element.Element):
+  def __init__(self, parent, description, interval=1, name='Element.Loop',
+               report_error_on_close=False, timeout=DEFAULT_TIMEOUT):
+    super(Loop, self).__init__(parent, description)
     self.name = name or repr(self)
     self.report_error_on_close = report_error_on_close
     self.interval = interval
-    self.next_time = next_time
-    self.loop_target = loop_target
-
     self.timeout = timeout
     self.pause_time = 0
     self.load_time = time.time()
+
+  def next_time(self, t):
+    return t + self.time_loop.interval
+
+  def loop_target(self, t):
+    pass
 
   def target(self):
     while self.is_running:
@@ -43,11 +41,11 @@ class TimeLoop(MasterRunnable):
         self.pause()
 
   def _on_pause(self):
-    super(TimeLoop, self)._on_pause()
+    super(Loop, self)._on_pause()
     self.pause_time = time.time() - self.start_time
 
   def _on_run(self):
-    super(TimeLoop, self)._on_run()
+    super(Loop, self)._on_run()
     t = time.time()
     self.start_time = t - self.pause_time
     self.next_loop_time = self.next_time(t)
@@ -71,24 +69,3 @@ class TimeLoop(MasterRunnable):
         time.sleep(sleep_time)
       else:
         LOGGER.error('Sleeping for negative time %s', sleep_time, limit=10)
-
-class Loop(Element.Element):
-  def __init__(self, parent, description, interval=1, name='Element.Loop'):
-    super(Loop, self).__init__(parent, description)
-    self.time_loop = TimeLoop(
-      name=name,
-      interval=interval,
-      next_time=getattr(self, 'next_time', None),
-      loop_target=getattr(self, 'loop_target', None))
-
-    self.add_slave(self.time_loop)
-    self.time_loop.add_pause_only_slave(self)
-
-  def next_time(self, t):
-    return t + self.time_loop.interval
-
-  def loop_target(self, t):
-    pass
-
-  # You can implement Loop.next_time and Loop.loop_target to override the
-  # methods in TimeLoop.
