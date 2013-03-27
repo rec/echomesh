44e8143e95f3191465641cda4581ec34473f2827
Tom Ritchford <tom@swirly.com>
2013-03-26 21:48:34 -0400

    Tiny tweak to Expressions

--- code/python/echomesh/util/math/Expressions.py
+++ code/python/echomesh/util/math/Expressions.py
@@ -85,18 +85,18 @@ FUNCTIONS = {
   'sgn': lambda a: abs(a) > EPSILON and cmp(a,0) or 0,
   }
 
-def evaluateStack(stack, variable_evaluator):
+def evaluateStack(stack, variables):
   op = stack.pop()
 
   if op.startswith('$'):
-    return variable_evaluator(op[1:])
+    return variables(op[1:])
 
   if op == 'unary -':
-    return -evaluateStack(stack, variable_evaluator)
+    return -evaluateStack(stack, variables)
 
   if op in OPERATORS:
-    op2 = evaluateStack(stack, variable_evaluator)
-    op1 = evaluateStack(stack, variable_evaluator)
+    op2 = evaluateStack(stack, variables)
+    op1 = evaluateStack(stack, variables)
     return OPERATORS[op](op1, op2)
 
   if op == 'PI':
@@ -106,7 +106,7 @@ def evaluateStack(stack, variable_evaluator):
     return math.e  # 2.718281828
 
   if op in FUNCTIONS:
-    return FUNCTIONS[op](evaluateStack(stack, variable_evaluator))
+    return FUNCTIONS[op](evaluateStack(stack, variables))
 
   if op[0].isalpha():
     raise Exception('invalid identifier "%s"' % op)
@@ -119,12 +119,49 @@ def evaluateStack(stack, variable_evaluator):
 
   return int(op)
 
-# class Evaluator(ob
+class Evaluator(object):
+  def __init__(stack, variables):
+    self.stack = stack[:]
+    self.variables = variables
+
+  def evaluate(self):
+    op = self.stack.pop()
+
+    if op.startswith('$'):
+      return self.variables(op[1:])
+
+    if op == 'unary -':
+      return -self.evaluate()
+
+    if op in OPERATORS:
+      op2 = self.evaluate()
+      op1 = self.evaluate()
+      return OPERATORS[op](op1, op2)
+
+    if op == 'PI':
+      return math.pi # 3.1415926535
+
+    if op == 'E':
+      return math.e  # 2.718281828
+
+    if op in FUNCTIONS:
+      return FUNCTIONS[op](self.evaluate())
+
+    if op[0].isalpha():
+      raise Exception('invalid identifier "%s"' % op)
+
+    if op.startswith('0x') or op.startswith('0X'):
+      return int(op, 16)
+
+    if '.' in op or 'e' in op or 'E' in op:
+      return float(op)
+
+    return int(op)
 
 class Expression(object):
-  def __init__(self, expression, variable_evaluator=None):
+  def __init__(self, expression, variables=None):
     self.expression = expression
-    self.variable_evaluator = variable_evaluator
+    self.variables = variables
     self.is_variable = ('$' in expression)
     self.stack = []
     bnf(self.stack).parseString(expression, parseAll=True)
@@ -132,7 +169,7 @@ class Expression(object):
 
   def evaluate(self):
     if self.is_variable or self.value is None:
-      self.value = evaluateStack(self.stack[:], self.variable_evaluator)
+      self.value = evaluateStack(self.stack[:], self.variables)
     return self.value
 
 def evaluate(expression):
