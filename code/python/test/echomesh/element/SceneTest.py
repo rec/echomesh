6560acc1d37ecdba30d00f37ee00730a159193ea
Tom Ritchford <tom@swirly.com>
2013-03-29 20:32:05 -0400

    Tiny tweaks before presenting my doctest issue

--- code/python/test/echomesh/element/SceneTest.py
+++ code/python/test/echomesh/element/SceneTest.py
@@ -1,19 +1,23 @@
 """
->>> scene = Scene.scene(None, TEST_DATA)
+# >>> scene = Scene.scene(None, TEST_DATA)
 
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from echomesh.base import Yaml
 from echomesh.element import Scene
+from echomesh.base import Yaml
 
 TEST_DATA = Yaml.decode_one("""
-type: insert
-offset: 5
-scene:
-  type: spread
-  begin: red
-  end: white
-  steps: 10
-""")
+  type: insert
+  offset: 5
+  scene:
+    type: spread
+    begin: red
+    end: white
+    steps: 10
+  """)
+
+if __name__ == '__main__':
+  import doctest
+  doctest.testmod()
