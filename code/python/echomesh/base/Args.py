a650b3a6dd1dc33fa846b962976f5d51daaae1bd
Tom Ritchford <tom@swirly.com>
2013-04-07 19:36:52 -0400

    Start to improve args.

--- code/python/echomesh/base/Args.py
+++ code/python/echomesh/base/Args.py
@@ -1,15 +1,17 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from echomesh.base import Enum
+
 ASSIGNMENT_ARGS = []
 
+_LEFT = '{[('
+_RIGHT_TO_LEFT = dict(zip('}])', _LEFT))
+
 def set_arguments(argv):
-  args = argv[1:]
-  if args:
-    a = args[0].strip()
-    if a:
-      ASSIGNMENT_ARGS[:] = split_args(args)
+  ASSIGNMENT_ARGS[:] = split_args(argv[1:])
 
 def split_args(args):
+  results = []
   address = []
   value = None
   equals_found = False
@@ -30,10 +32,109 @@ def split_args(args):
 
     if value:
       clean_address = filter(None, '.'.join(address).split('.'))
-      yield clean_address, value
+      results.append([clean_address, value])
       address = []
       equals_found = False
 
   if address:
     print('ERROR: Extra arguments at the end: "%s".' % ' '.join(address))
+  return results
 
+
+# Assignment looks like this:
+#  some.name.here = "123"  other.name.here = [1, 2]
+
+States = Enum.enum(
+  'BEFORE_ADDRESS',
+  'IN_ADDRESS',
+  'BEFORE_EQUALS',
+  'BEFORE_VALUE',
+  'IN_VALUE',
+  )
+
+# Handles quotation marks, brackets, etc.
+def split_args2(s):
+  results = []
+  in_quotes = False
+  backslashed = False
+  state = States.BEFORE_ADDRESS
+  bracket_stack = []
+  address = []
+  value = []
+
+  for col, ch in enumerate(s):
+    def error(s):
+      raise Exception('On column %d, %s.' %s (col, s))
+    perhaps_done = False
+
+    if state is State.BEFORE_ADDRESS:
+      if ch.isalpha():
+        address.append(ch)
+        state = State.IN_ADDRESS
+      elif not ch.isspace():
+        error('Expected a letter, not "%s"' % ch)
+
+    if state is State.IN_ADDRESS:
+      if ch.isalpha() or ch == '.':
+        address.append(ch)
+      elif ch.isspace():
+        state = State.BEFORE_EQUALS
+      elif ch == '=':
+        state = State.BEFORE_VALUE
+
+    if state is State.BEFORE_EQUALS:
+      if ch == '=':
+        state = State.BEFORE_VALUE
+      elif not ch.isspace():
+        error('Expected "=", not "%s"' % ch)
+      continue
+
+    if state is State.BEFORE_VALUE:
+      if ch.isspace():
+        continue
+      state = State.IN_VALUE
+
+    if backslashed:
+      backslashed = False
+
+    elif ch == '\\':
+      backslashed = True
+      continue
+
+    elif ch == '"':
+      if in_quotes:
+        in_quotes = False
+        perhaps_done = True
+      else:
+        in_quotes = True
+
+    elif in_quotes:
+      pass
+
+    elif ch.isspace():
+      if not bracket_stack:
+        perhaps_done = True
+
+    elif ch in _LEFT:
+      bracket_stack.append(ch)
+
+    elif ch in _RIGHT_TO_LEFT:
+      left = _RIGHT_TO_LEFT[ch]
+      if not bracket_stack:
+        error('Closing %s without opening %s' % (ch, left))
+      top = bracket_stack.pop()
+      if top != left:
+        error('Got closing %s for opening %s' % (left, top))
+      perhaps_done = True
+
+    value.append(ch)
+    last_time = col == (len(s) - 1)
+    if perhaps_done or last_time:
+      if bracket_stack:
+        if last_time:
+          error('Missing closing brackets for %s' % ''.join(bracket_stack))
+      else:
+        address = []
+        value = []
+        results.append([''.join(address), ''.join(value)])
+  return results
--- code/python/test/echomesh/base/ArgsTest.py
+++ code/python/test/echomesh/base/ArgsTest.py
@@ -1,24 +1,24 @@
 """
->>> list(split_args([]))
+>>> split_args([])
 []
 
->>> list(split_args(['extra=32']))
-[([u'extra'], u'32')]
+>>> split_args(['extra=32'])
+[[[u'extra'], u'32']]
 
->>> list(split_args(['extra=', '32']))
-[([u'extra'], u'32')]
+>>> split_args(['extra=', '32'])
+[[[u'extra'], u'32']]
 
->>> list(split_args(['extra', '=32']))
-[([u'extra'], u'32')]
+>>> split_args(['extra', '=32'])
+[[[u'extra'], u'32']]
 
->>> list(split_args(['extra', '=', '32']))
-[([u'extra'], u'32')]
+>>> split_args(['extra', '=', '32'])
+[[[u'extra'], u'32']]
 
->>> list(split_args(['ac.', 'de.', '=', '.defeg.']))
-[([u'ac', u'de'], u'.defeg.')]
+>>> split_args(['ac.', 'de.', '=', '.defeg.'])
+[[[u'ac', u'de'], u'.defeg.']]
 
->>> list(split_args(['extra=', '32 men', 'ac', '.de.', '=', '.defeg.']))
-[([u'extra'], u'32 men'), ([u'ac', u'de'], u'.defeg.')]
+>>> split_args(['extra=', '32 men', 'ac', '.de.', '=', '.defeg.'])
+[[[u'extra'], u'32 men'], [[u'ac', u'de'], u'.defeg.']]
 
 
 """
