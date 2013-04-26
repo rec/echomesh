2bde9b1098a5acb52c70648d9d2ef88eb42bca6c
Tom Ritchford <tom@swirly.com>
2013-04-25 22:55:39 -0400

    Split echomesh/component .cpp files

--- code/cpp/echomesh/component/Instrument.h
+++ code/cpp/echomesh/component/Instrument.h
@@ -8,48 +8,15 @@ namespace echomesh {
 
 class Instrument : public Component {
  public:
-  Instrument() : color_(Colours::black), labelColor_(Colours::white) {}
+  Instrument();
   ~Instrument() {}
 
-  void configure(const String& label, const LightDisplay& config) {
-    MessageManagerLock l;
-    config_ = config;
-    label_ = label;
-    repaint();
-  }
+  void configure(const String& label, const LightDisplay&);
 
-  void setColor(uint8 r, uint8 g, uint8 b) {
-    if (r != color_.getRed() ||
-        g != color_.getGreen() ||
-        b != color_.getBlue()) {
-      MessageManagerLock l;
-      color_ = Colour(r, g, b);
-      static const int GREY = (3 * 0xFF) / 2;
-      labelColor_ = ((r + g + b) >= GREY) ? Colours::black : Colours::white;
-      repaint();
-    }
-  }
+  void setColor(uint8 r, uint8 g, uint8 b);
 
-  void setColor(const Colour& c) {
-    setColor(c.getRed(), c.getGreen(), c.getBlue());
-  }
-
-  virtual void paint(Graphics& g) {
-    log("painting Instrument");
-    g.setColour(color_);
-    Rectangle<int> b = getLocalBounds();
-    if (config_.isRect)
-      g.fillRect(b);
-    else
-      g.fillEllipse(b.getX(), b.getY(), b.getWidth(), b.getHeight());
-
-    if (config_.label) {
-      log("writing the label!!!");
-      b.reduce(config_.labelPadding.x / 2, config_.labelPadding.y / 2);
-      g.setColour(labelColor_);
-      g.drawFittedText(label_, b, Justification::centred, 1);
-    }
-  }
+  void setColor(const Colour&);
+  virtual void paint(Graphics&);
 
  private:
   String label_;
--- code/cpp/echomesh/component/InstrumentGrid.h
+++ code/cpp/echomesh/component/InstrumentGrid.h
@@ -4,57 +4,17 @@
 #include "echomesh/base/Echomesh.h"
 #include "echomesh/base/LightConfig.h"
 #include "echomesh/component/Instrument.h"
-#include "rec/util/STL.h"
 
 namespace echomesh {
 
 class InstrumentGrid : public Component {
  public:
-  InstrumentGrid() { setSize(64, 64); }
-  ~InstrumentGrid() { rec::stl::deletePointers(&instruments_); }
+  InstrumentGrid();
+  virtual ~InstrumentGrid();
 
-  void setConfig(const LightConfig& config) {
-    MessageManagerLock l;
-    config_ = config;
-    int oldCount = instruments_.size();
-
-    for (int i = config_.count; i < oldCount; ++i)
-      delete instruments_[i];
-    instruments_.resize(config_.count);
-    for (int i = oldCount; i < config_.count; ++i)
-      instruments_[i] = new Instrument;
-
-    const LightDisplay& light = config_.display.light;
-    int delta = light.labelStartsAtZero ? 0 : 1;
-    for (int i = 0; i < instruments_.size(); ++i)
-      instruments_[i]->configure(String(i + delta), light);
-
-    int top = config_.display.padding.top;
-    int left = config_.display.padding.left;
-    int columns = config.display.layout.x;
-    int rows = config.display.layout.y;
-
-    int w = light.size.x;
-    int h = light.size.y;
-    int index = 0;
-    for (int y = 0; y < rows; ++y) {
-      for (int x = 0; x < columns; ++x)
-        instruments_[index++]->setBounds(left + x * w, top + y * h, w, h);
-    }
-
-    setSize(left + w * columns + config_.display.padding.right,
-            top + h * rows + config_.display.padding.bottom);
-    repaint();
-  }
-
-  void setLights(const ColorList& lights) {
-    for (int i = 0; i < lights.size(); ++i)
-      instruments_[i]->setColor(lights[i]);
-  }
-
-  void paint(Graphics& g) {
-    g.fillAll(config_.display.background);
-  }
+  void setConfig(const LightConfig&);
+  void setLights(const ColorList&);
+  void paint(Graphics&);
 
  private:
   LightConfig config_;
