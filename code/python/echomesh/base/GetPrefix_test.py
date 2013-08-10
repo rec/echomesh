48df47375cd5c3a27a3a74a39f9930d01d9a30a8
Tom Ritchford <tom@swirly.com>
2013-08-10 02:13:19 -0400

    Move GetPrefixTest and add missing TestCase.

---
+++ code/python/echomesh/base/GetPrefix_test.py
@@ -0,0 +1,56 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import math
+
+from echomesh.base import GetPrefix
+from echomesh.util.TestCase import TestCase
+
+TABLE = {'one': 1, 'two': 2, 'three': 3}
+MASTER = {'foo': {'bar': {'baz': 23}}, 'new': {}}
+
+class GetPrefixTest(TestCase):
+  def setUp(self):
+    self.slave = {}
+
+  def test_simple(self):
+    self.assertEqual(GetPrefix.get_prefix(TABLE, 'one'), ('one', 1))
+
+  def test_prefix(self):
+    self.assertEqual(GetPrefix.get_prefix(TABLE, 'o'), ('one', 1))
+
+  def test_no_prefix(self):
+    self.assertEqual(GetPrefix.get_prefix(TABLE, 'one', allow_prefixes=False),
+                     ('one', 1))
+
+  def test_bad_prefix(self):
+    try:
+      GetPrefix.get_prefix(TABLE, 'o', allow_prefixes=False)
+    except Exception as e:
+      self.assertEqual(str(e), '"o" is not valid.')
+
+  def test_no_prefix(self):
+    try:
+      GetPrefix.get_prefix(TABLE, 'x', allow_prefixes=False)
+    except Exception as e:
+      self.assertEqual(str(e), '"x" is not valid.')
+
+  def test_many_prefixes(self):
+    try:
+      GetPrefix.get_prefix(TABLE, 't')
+    except Exception as e:
+      self.assertEqual(str(e),
+                       '"t" matches more than one: three and two.')
+
+  def test_assignment(self):
+    GetPrefix.set_assignment('foo.bar.baz', 32, MASTER, self.slave)
+    self.assertEqual(self.slave, {'foo': {'bar': {'baz': 32}}})
+
+  def test_assignment_prefix(self):
+    GetPrefix.set_assignment('f.b.b', 19, MASTER, self.slave)
+    self.assertEqual(self.slave, {'foo': {'bar': {'baz': 19}}})
+
+  def test_assignment_unmapped(self):
+    GetPrefix.set_assignment('new.foo', 23, MASTER, self.slave,
+                             unmapped_names=set(['new']))
+    self.assertEqual(self.slave, {'new': {'foo': 23}})
+
---
+++ code/python/echomesh/util/TestCase.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import unittest
+
+class TestCase(unittest.TestCase):
+  EPSILON = 0.000001
+
+  def assertNear(self, x, y, msg=None):
+    self.assertTrue(abs(x - y) < self.EPSILON, msg or ('%s != %s' % (x, y)))
+
+  def assertArrayEquals(self, x, y):
+    self.assertEqual(x.shape, y.shape)
+    columns, rows = x.shape
+    for i in range(columns):
+      for j in range(rows):
+        msg = 'for [%d][%d], %s != %s' % (i, j, x[i][j], y[i][j])
+        self.assertNear(x[i][j], y[i][j], msg)
+
+
