f95f9c0b514377c110451e8f3f51f1c2bc230a3b
Tom Ritchford <tom@swirly.com>
2013-10-21 22:54:15 -0400

    Fix unused code paths.

--- code/python/echomesh/base/Leafs.py
+++ code/python/echomesh/base/Leafs.py
@@ -4,7 +4,7 @@ def leafs(table):
   values = {}
   def recurse(item, path):
     if isinstance(item, dict):
-      for key, value in six.iteritems(item):
+      for key, value in item.items():
         recurse(value, path + (key,))
     else:
       values[path] = item
