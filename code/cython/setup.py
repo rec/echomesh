a502eccf4180eed896c73b5edb92bb300b0ca8aa
Tom Ritchford <tom@swirly.com>
2013-11-07 02:00:54 -0500

    Changes for Cython mailing list.

--- code/cython/setup.py
+++ code/cython/setup.py
@@ -8,6 +8,10 @@ import platform
 
 from distutils.core import setup, Command, Extension
 from Cython.Build import cythonize
+from Cython.Distutils import extension
+
+import Cython.Build
+print(help(Cython.Build))
 
 DEBUG = True
 
@@ -19,14 +23,16 @@ IS_WINDOWS = (PLATFORM == 'windows')
 
 assert IS_LINUX or IS_MAC or IS_WINDOWS
 
+EXTRA_ARGS = {'cython_gdb': True} if DEBUG else {}
+
 if IS_MAC:
   EXTRA_COMPILE_ARGS = ('-x c++ -arch x86_64 -fmessage-length=0 -std=c++11 '
                         '-IJuceLibraryCode'.split(' '))
   EXTRA_LINK_ARGS = '-framework Cocoa -framework WebKit'.split(' ')
 
   if DEBUG:
-    EXTRA_COMPILE_ARGS += ('-O0 -D_DEBUG=1 -DDEBUG=1 ' +
-                           '-DJUCER_XCODE_MAC_F6D2F4CF=1').split(' ')
+    EXTRA_COMPILE_ARGS += ('-O0 -g -D_DEBUG=1 -DDEBUG=1').split(' ')
+    EXTRA_LINK_ARGS += ['-g']
     LIBRARY = 'echomesh-debug'
     LIB_DIR = '/development/echomesh/code/cython/Builds/MacOSX/build/Debug'
 
@@ -35,23 +41,18 @@ if IS_MAC:
     LIBRARY = 'echomesh'
     LIB_DIR = '/development/echomesh/code/cython/Builds/MacOSX/build/Release'
 
-elif IS_LINUX:
-  raise Exception("Can't build for Linux")
-
-elif IS_WINDOWS:
-  raise Exception("Can't build for Windows")
-
 else:
   raise Exception("Don't understand platform %s." % platform)
 
-lib = Extension(
+lib = extension.Extension(
   LIBRARY,
   ['echomesh.pyx'],
   library_dirs=[LIB_DIR],
   libraries=['echomesh'],
   extra_compile_args=EXTRA_COMPILE_ARGS,
   extra_link_args=EXTRA_LINK_ARGS,
-  )
+  cython_gdb=True)
+#  **EXTRA_ARGS)
 
 class CleanCommand(Command):
   description = "custom clean command that forcefully removes dist/build directories"
@@ -66,8 +67,12 @@ class CleanCommand(Command):
     assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
     os.system('rm -Rf %s.so ./build ./dist' % LIBRARY)
 
+
 setup(
   name='Echomesh',
-  ext_modules=cythonize([lib]),
+#  ext_modules=[lib],
+#  ext_modules=cythonize([lib]),
+#  ext_modules=cythonize([lib], **EXTRA_ARGS),
+  ext_modules=cythonize([lib], cython_gdb=True),
   cmdclass={'clean': CleanCommand},
   )
