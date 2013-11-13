433f554bb8d69fe54e35a5ff796f73ee02fb8a02
Tylor Steinberger <tlsteinberger167@gmail.com>
2013-11-12 20:12:45 -0500

    Linux support for Video

--- code/python/echomesh/element/Video.py
+++ code/python/echomesh/element/Video.py
@@ -5,11 +5,15 @@ from echomesh.util import Log
 
 LOGGER = Log.logger(__name__)
 
-DEFAULT_PLAYER = '/usr/bin/omxplayer'
+DEFAULT_PLAYER = '/usr/bin/cvlc'
 
 class Video(Execute):
   def __init__(self, parent, description):
     if 'binary' not in description:
       description['binary'] = DEFAULT_PLAYER
     super(Video, self).__init__(parent, description)
+    args = description.get('args', [])
+    self.command_line = [description['binary'], '--fullscreen', '--overlay',
+                         '--key-leave-fullscreen', "", '--video-on-top', '--play-and-stop', '%s' % args]
+    print(self.command_line)
 
