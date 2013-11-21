eb0b5d7f50a9a6eb2378a048b78e22a90f05b0b9
Tom Ritchford <tom@swirly.com>
2013-11-21 01:21:16 -0500

    Starting to play through cechomesh

--- code/python/echomesh/sound/CPlayer.py
+++ code/python/echomesh/sound/CPlayer.py
@@ -1,20 +1,23 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from cechomesh.AudioPlayer import AudioPlayer
+import cechomesh
 
 from echomesh.sound import PlayerSetter
 from echomesh.util import Log
+from echomesh.util.thread.Runnable import Runnable
 
 LOGGER = Log.logger(__name__)
 
 class CPlayer(Runnable):
-  def __init__(self, element, level=1, pan=0, loops=1, length=_INF, **kwds):
+  def __init__(self, element, level=1, pan=0, loops=1, length=-float('inf'),
+               **kwds):
     super(CPlayer, self).__init__()
     PlayerSetter.evaluate_player(
       self, element, level=level, pan=pan, loops=loops, length=length, **kwds)
-    player = AudioPlayer(self)
+    player = cechomesh.AudioSource(
+      self._filename, self._loops, self._begin, self._end,
+      self._length, "", 2, self._level, self._pan)
     self._on_run = player.run
     self._on_stop = player.stop
     self._on_stop = player.pause
     self.unload = player.unload
-
--- code/python/echomesh/sound/Play.py
+++ code/python/echomesh/sound/Play.py
@@ -12,10 +12,6 @@ def _get_player(player):
     from echomesh.sound import AplayPlayer
     return AplayPlayer.AplayPlayer
 
-  if player == 'pyaudio':
-    from echomesh.sound import PyaudioPlayer
-    return PyaudioPlayer.PyaudioPlayer
-
   if player == 'client':
     from echomesh.sound import ClientPlayer
     return ClientPlayer.ClientPlayer
@@ -23,6 +19,10 @@ def _get_player(player):
   if player == 'cython':
     from echomesh.sound import CPlayer
     return CPlayer.CPlayer
+
+  if player == 'pyaudio':
+    from echomesh.sound import PyaudioPlayer
+    return PyaudioPlayer.PyaudioPlayer
 
   raise Exception('Don\'t understand player %s' % player)
 
