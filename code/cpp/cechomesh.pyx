20e1260df3bdd93ea1b58d7976d260626a3e037f
Tom Ritchford <tom@swirly.com>
2013-12-03 19:52:35 -0500

    More breaking apart and move around the Cython code.

--- code/cpp/cechomesh.pyx
+++ code/cpp/cechomesh.pyx
@@ -1,7 +1,7 @@
 # distutils: language = c++
 # cython: c_string_type=unicode, c_string_encoding=utf8
 
-include "application.pyx"
+include "echomesh/util/EchomeshApplication.pyx"
 include "audio.pyx"
 include "echomesh/component/LightingWindow.pyx"
 include "log.pyx"
--- code/cpp/echomesh/util/EchomeshApplication.pyx
+++ code/cpp/echomesh/util/EchomeshApplication.pyx
@@ -1,11 +1,10 @@
-include "callback.pyx"
+include "echomesh/util/AppCallback.pyx"
 
 from libcpp cimport bool
 from libcpp.string cimport string
 
 cdef extern from "echomesh/util/EchomeshApplication.h" namespace "echomesh":
   void startApplication(VoidCaller cb, void* user_data) nogil
-  bool isStarted()
 
 cdef extern from "echomesh/util/Quit.h" namespace "echomesh":
   void quit() nogil
@@ -20,6 +19,3 @@ def start_application(f):
 
 def stop_application():
   quit()
-
-def is_started():
-  return isStarted()
