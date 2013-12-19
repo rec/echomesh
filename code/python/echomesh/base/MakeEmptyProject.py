2a4a0bb0dbe3604af0d610b5b496acf49bf5f10e
Tom Ritchford <tom@swirly.com>
2013-12-19 12:44:42 -0500

    Change bank to output

--- code/python/echomesh/base/MakeEmptyProject.py
+++ code/python/echomesh/base/MakeEmptyProject.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import copy
 import os.path
 
+from echomesh.base import EmptyProject
 from echomesh.base import MakeDirs
 from echomesh.base import Name
 
@@ -28,10 +29,9 @@ _PATH_FORMAT = '  %s'
 def _print_path(p):
   print(_PATH_FORMAT % p)
 
-def make_empty_project(path):
+def _make_empty_project(path):
   """Make an empty echomesh project at this path."""
-  print()
-  ep = copy.deepcopy(EMPTY_PROJECT)
+  ep = copy.deepcopy(EmptyProject.EMPTY_PROJECT)
   name_text = _NAME % Name.NAME
   ep['data']['name'][Name.NAME] = {'config.yml': name_text, 'score': None}
   _make(path, ep)
@@ -61,59 +61,10 @@ def ask_to_make_empty_project(path):
     print(_CREATE_MISSING_DIRECTORY_PROJECT % path, end='')
     yn = raw_input().strip().lower()
   if not (yn and yn[0] == 'n'):
-    print(_PROGRESS_MESSAGE, end='')
-    make_empty_project(path)
+    print(_PROGRESS_MESSAGE)
+    _make_empty_project(path)
     return True
   else:
     print(_MISSING_DIRECTORY_ERROR % path)
     return False
 
-_CONFIG = """\
-# This is your master config file.
-# Put your configuration settings here.
-"""
-
-_README = """\
-This is an empty echomesh directory.
-
-Put your sound or image files in asset/image or asset/audio.
-Put your configurations in data/master/config.yml
-"""
-
-_NAME = """\
-# This is where you store configurations and scores that are only
-# used on this specific machine, %s.
-"""
-
-_SCORE = """\
-# This is a sample score.
-
-type: print
-text: Hello, world!
-
-"""
-
-EMPTY_PROJECT = {
-  'asset': {
-    'audio': None,
-    'image': None,
-  },
-
-  'cache': None,
-
-  'data': {
-    'master': {
-      'bank': None,
-      'config': { 'config.yml':  _CONFIG, },
-      'pattern': None,
-      'scene': None,
-      'score': { 'sample-score.yml': _SCORE, },
-    },
-
-    'name': {}
-  },
-
-  'log': None,
-
-  'README.txt': _README,
-}
