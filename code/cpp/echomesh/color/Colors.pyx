e73f7baa07d47e8c5a28091c66b55eb890c4e88d
Tom Ritchford <tom@swirly.com>
2013-12-28 17:16:23 -0500

    Yet more tiny color tweaks.

--- code/cpp/echomesh/color/Colors.pyx
+++ code/cpp/echomesh/color/Colors.pyx
@@ -1,6 +1,5 @@
 from libcpp.string cimport string
 
 cdef extern from "echomesh/color/Colors.h" namespace "echomesh::color":
-  FColor interpolate(FColor begin, FColor end, float ratio)
   void scaleFColorList(FColorList* fc, float scale)
   void combineFColorList(FColorList, FColorList*)
