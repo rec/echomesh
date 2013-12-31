c4dda4697a1f78f7dcaa1b51412b2e7f18251b62
Tom Ritchford <tom@swirly.com>
2013-12-31 12:02:48 -0500

    Start to create Transforms

---
+++ code/cpp/echomesh/color/Transform.cpp
@@ -0,0 +1,27 @@
+#include <map>
+
+#include "echomesh/color/Transform.h"
+
+namespace echomesh {
+namespace color {
+
+namespace {
+
+typedef std::map<string, FloatTransform> TransformMap;
+
+TransformMap makeTransforms() {
+  TransformMap tm;
+  return tm;
+}
+
+auto TRANSFORMS = makeTransforms();
+
+}
+
+const FloatTransform* getTransform(const string& name) {
+  auto i = TRANSFORMS.find(name);
+  return i == TRANSFORMS.end() ? nullptr : &i->second;
+}
+
+}  // namespace color
+}  // namespace echomesh
---
+++ code/cpp/echomesh/color/Transform.h
@@ -0,0 +1,20 @@
+#ifndef __ECHOMESH_COLOR_TRANSFORM__
+#define __ECHOMESH_COLOR_TRANSFORM__
+
+#include <functional>
+
+#include "echomesh/base/Echomesh.h"
+
+namespace echomesh {
+namespace color {
+
+typedef std::function<float(float)> FloatFunction;
+typedef std::pair<FloatFunction, FloatFunction> FloatTransform;
+
+const FloatTransform* getTransform(const string&);
+
+
+}  // namespace color
+}  // namespace echomesh
+
+#endif  // __ECHOMESH_COLOR_TRANSFORM__
