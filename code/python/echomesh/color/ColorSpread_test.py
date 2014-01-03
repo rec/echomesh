d83e4b421653a4d9b0148971e4723db9e042bd46
Tom Ritchford <tom@swirly.com>
2014-01-03 01:37:35 -0500

    Cleaned up unit tests.

--- code/python/echomesh/color/ColorSpread_test.py
+++ code/python/echomesh/color/ColorSpread_test.py
@@ -21,33 +21,61 @@ class TestColorSpread(TestCase):
     self.assertSpread('red', 'blue', 3, use_hsb=True)
 
   def test_one_spread(self):
-    self.result = ('[red, [red=1.000, green=0.000, blue=0.400], [red=1.000, green=0.000, blue=0.800], [red=0.800, green=0.000, blue=1.000], [red=0.400, green=0.000, blue=1.000], blue]')
+    self.result = ('[red, '
+                   '[red=1.000, green=0.000, blue=0.400], '
+                   '[red=1.000, green=0.000, blue=0.800], '
+                   '[red=0.800, green=0.000, blue=1.000], '
+                   '[red=0.400, green=0.000, blue=1.000], blue]')
     self.assertSpread('red', 'blue', 6, use_hsb=True)
 
   def test_no_hsb(self):
-    self.result = ('[red, [red=0.800, green=0.000, blue=0.200], [red=0.600, green=0.000, blue=0.400], [red=0.400, green=0.000, blue=0.600], [red=0.200, green=0.000, blue=0.800], blue]')
+    self.result = ('[red, '
+                   '[red=0.800, green=0.000, blue=0.200], '
+                   '[red=0.600, green=0.000, blue=0.400], '
+                   '[red=0.400, green=0.000, blue=0.600], '
+                   '[red=0.200, green=0.000, blue=0.800], blue]')
     self.assertSpread('red', 'blue', 6, use_hsb=False)
 
   def test_two_colors(self):
-    self.result = ('[yellow, [red=0.600, green=1.000, blue=0.000], [red=0.200, green=1.000, blue=0.000], [red=0.000, green=1.000, blue=0.200], [red=0.000, green=1.000, blue=0.600], cyan]')
+    self.result = ('[yellow, '
+                   '[red=0.600, green=1.000, blue=0.000], '
+                   '[red=0.200, green=1.000, blue=0.000], '
+                   '[red=0.000, green=1.000, blue=0.200], '
+                   '[red=0.000, green=1.000, blue=0.600], cyan]')
     self.assertSpread('yellow', 'cyan', 6)
 
   def test_two_colors_identity(self):
-    self.result = ('[yellow, [red=0.600, green=1.000, blue=0.000], [red=0.200, green=1.000, blue=0.000], [red=0.000, green=1.000, blue=0.200], [red=0.000, green=1.000, blue=0.600], cyan]')
+    self.result = ('[yellow, '
+                   '[red=0.600, green=1.000, blue=0.000], '
+                   '[red=0.200, green=1.000, blue=0.000], '
+                   '[red=0.000, green=1.000, blue=0.200], '
+                   '[red=0.000, green=1.000, blue=0.600], cyan]')
     self.assertSpread('yellow', 'cyan', 6, 'identity')
 
   def test_two_colors_square(self):
-    self.result = ('[yellow, [red=0.920, green=1.000, blue=0.000], [red=0.680, green=1.000, blue=0.000], [red=0.280, green=1.000, blue=0.000], [red=0.000, green=1.000, blue=0.280], cyan]')
+    self.result = ('[yellow, '
+                   '[red=0.920, green=1.000, blue=0.000], '
+                   '[red=0.680, green=1.000, blue=0.000], '
+                   '[red=0.280, green=1.000, blue=0.000], '
+                   '[red=0.000, green=1.000, blue=0.280], cyan]')
     self.assertSpread(
       'yellow', 'cyan', 6, transform='square')
 
   def test_two_colors_sqrt(self):
-    self.result = ('[yellow, [red=0.106, green=1.000, blue=0.000], [red=0.000, green=1.000, blue=0.265], [red=0.000, green=1.000, blue=0.549], [red=0.000, green=1.000, blue=0.789], cyan]')
+    self.result = ('[yellow, '
+                   '[red=0.106, green=1.000, blue=0.000], '
+                   '[red=0.000, green=1.000, blue=0.265], '
+                   '[red=0.000, green=1.000, blue=0.549], '
+                   '[red=0.000, green=1.000, blue=0.789], cyan]')
     self.assertSpread('yellow', 'cyan', 6,
                             transform='square+inverse')
 
   def test_inverse_square(self):
-    self.result = ('[yellow, [red=0.106, green=1.000, blue=0.000], [red=0.000, green=1.000, blue=0.265], [red=0.000, green=1.000, blue=0.549], [red=0.000, green=1.000, blue=0.789], cyan]')
+    self.result = ('[yellow, '
+                   '[red=0.106, green=1.000, blue=0.000], '
+                   '[red=0.000, green=1.000, blue=0.265], '
+                   '[red=0.000, green=1.000, blue=0.549], '
+                   '[red=0.000, green=1.000, blue=0.789], cyan]')
     self.assertSpread('yellow', 'cyan',
                             6, transform='square+inverse')
   def test_exp(self):
@@ -60,18 +88,34 @@ class TestColorSpread(TestCase):
     self.assertSpread('yellow', 'cyan', 6, transform='exp')
 
   def test_inverse_exp(self):
-    self.result = ('[yellow, [red=0.409, green=1.000, blue=0.000], [red=0.000, green=1.000, blue=0.046], [red=0.000, green=1.000, blue=0.417], [red=0.000, green=1.000, blue=0.730], cyan]')
+    self.result = ('[yellow, '
+                   '[red=0.409, green=1.000, blue=0.000], '
+                   '[red=0.000, green=1.000, blue=0.046], '
+                   '[red=0.000, green=1.000, blue=0.417], '
+                   '[red=0.000, green=1.000, blue=0.730], cyan]')
     self.assertSpread(
       'yellow', 'cyan', 6, transform='exp+inverse')
 
   def test_sine(self):
-    self.result = ('[yellow, [red=0.809, green=1.000, blue=0.000], [red=0.309, green=1.000, blue=0.000], [red=0.000, green=1.000, blue=0.309], [red=0.000, green=1.000, blue=0.809], cyan]')
+    self.result = ('[yellow, '
+                   '[red=0.809, green=1.000, blue=0.000], '
+                   '[red=0.309, green=1.000, blue=0.000], '
+                   '[red=0.000, green=1.000, blue=0.309], '
+                   '[red=0.000, green=1.000, blue=0.809], cyan]')
     self.assertSpread('yellow', 'cyan', 6, transform='sine')
 
   def test_inverse_sine(self):
-    self.result = ('[yellow, [red=0.410, green=1.000, blue=0.000], [red=0.128, green=1.000, blue=0.000], [red=0.000, green=1.000, blue=0.128], [red=0.000, green=1.000, blue=0.410], cyan]')
+    self.result = ('[yellow, '
+                   '[red=0.410, green=1.000, blue=0.000], '
+                   '[red=0.128, green=1.000, blue=0.000], '
+                   '[red=0.000, green=1.000, blue=0.128], '
+                   '[red=0.000, green=1.000, blue=0.410], cyan]')
     self.assertSpread('yellow', 'cyan', 6, transform='sine+inverse')
 
   def test_name(self):
-    self.result = ('[red, [red=1.000, green=0.200, blue=0.200], [red=1.000, green=0.400, blue=0.400], [red=1.000, green=0.600, blue=0.600], [red=1.000, green=0.800, blue=0.800], white]')
+    self.result = ('[red, '
+                   '[red=1.000, green=0.200, blue=0.200], '
+                   '[red=1.000, green=0.400, blue=0.400], '
+                   '[red=1.000, green=0.600, blue=0.600], '
+                   '[red=1.000, green=0.800, blue=0.800], white]')
     self.assertSpread('red', 'white', 6)
