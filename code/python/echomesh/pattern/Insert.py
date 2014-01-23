3b53ef65ebcf0b33573872172a0177a2b2cf94e7
Tom Ritchford <tom@swirly.com>
2014-01-23 15:21:36 -0500

    Continue to refactor Insert.

--- code/python/echomesh/pattern/Insert.py
+++ code/python/echomesh/pattern/Insert.py
@@ -4,20 +4,19 @@ from echomesh.base import Config
 from echomesh.pattern.Maker import maker
 
 @maker('offset', 'length', 'rollover', 'skip')
-def insert(color_lists, target=None, offset=None, length=None, rollover=True,
-           skip=None):
+def insert(color_lists, offset=None, length=None, rollover=True, skip=None):
   assert len(color_lists) == 1
   color_list = color_lists[0]
 
   skip = int(skip or 1)
   offset = int((offset and offset.evaluate()) or 0)
   if length is None:
-    length = Config.get('light', 'count') if target is None else len(target)
+    length = Config.get('light', 'count')
 
-  return _insert(color_list, target, offset, length, rollover, skip)
+  return _insert(color_list, offset, length, rollover, skip)
 
-def _insert(color_list, target, offset, length, rollover, skip):
-  result = target or ([None] * length)
+def _insert(color_list, offset, length, rollover, skip):
+  result = [None] * length
   for i, light in enumerate(color_list):
     index = offset + i
     if index < 0 or index >= length:
