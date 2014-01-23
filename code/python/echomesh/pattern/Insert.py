13d22d3275e388027685412191778bac7a3fd92d
Tom Ritchford <tom@swirly.com>
2014-01-23 15:57:22 -0500

    Insert is now Cythonized.

--- code/python/echomesh/pattern/Insert.py
+++ code/python/echomesh/pattern/Insert.py
@@ -1,29 +1,20 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import cechomesh
+
 from echomesh.base import Config
 from echomesh.pattern.Maker import maker
+
+USE_CECHOMESH = True
 
 @maker('offset', 'length', 'rollover', 'skip')
 def insert(color_lists, offset=None, length=None, rollover=True, skip=None):
   assert len(color_lists) == 1
   color_list = color_lists[0]
 
-  skip = int(skip or 1)
+  skip = int((skip and skip.evaluate()) or 1)
   offset = int((offset and offset.evaluate()) or 0)
   if length is None:
     length = Config.get('light', 'count')
 
-  return _insert(color_list, offset, length, rollover, skip)
-
-def _insert(color_list, offset, length, rollover, skip):
-  result = [None] * length
-  for i, light in enumerate(color_list):
-    index = offset + i
-    if index < 0 or index >= length:
-      if rollover:
-        index = index % length
-      else:
-        continue
-    result[index] = light
-
-  return result
+  return cechomesh.insert_color_list(color_list, offset, length, rollover, skip)
