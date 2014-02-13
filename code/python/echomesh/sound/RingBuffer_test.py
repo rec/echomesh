c1e6209d6f8705d26db06b190daff9cfdc742302
Tom Ritchford <tom@swirly.com>
2014-02-13 18:44:43 -0500

    Another test for RingBuffer.

--- code/python/echomesh/sound/RingBuffer_test.py
+++ code/python/echomesh/sound/RingBuffer_test.py
@@ -6,7 +6,7 @@ import cechomesh
 
 class RingBuffer_test(TestCase):
   def setUp(self):
-    self.buffer = cechomesh.AudioRingBuffer(1, 32)
+    self.buffer = cechomesh.AudioRingBuffer(1, 16)
 
   def test_empty(self):
     self.assertEqual(self.buffer.available(), 0)
@@ -14,5 +14,9 @@ class RingBuffer_test(TestCase):
     self.assertEqual(self.buffer.read(16, data), 0)
     self.assertEqual(data, [[]])
 
-
+  def test_all(self):
+    self.assertEqual(self.buffer.write([range(16)]), 16)
+    data = []
+    self.assertEqual(self.buffer.read(16, data), 16)
+    self.assertEqual(data, [[float(i) for i in range(16)]])
 
