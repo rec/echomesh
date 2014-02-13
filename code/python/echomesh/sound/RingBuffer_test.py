9f12e0bb90f04e8e52bd8f84bc949711fc820563
Tom Ritchford <tom@swirly.com>
2014-02-13 18:47:37 -0500

    last time for RingBuffer

--- code/python/echomesh/sound/RingBuffer_test.py
+++ code/python/echomesh/sound/RingBuffer_test.py
@@ -20,3 +20,16 @@ class RingBuffer_test(TestCase):
     self.assertEqual(self.buffer.read(16, data), 16)
     self.assertEqual(data, [[float(i) for i in range(16)]])
 
+  def test_wrap_read(self):
+    self.assertEqual(self.buffer.write([range(12)]), 12)
+    data = []
+    self.assertEqual(self.buffer.read(8, data), 8)
+    self.assertEqual(data, [[float(i) for i in range(8)]])
+
+    self.assertEqual(self.buffer.write([range(8)]), 8)
+    self.assertEqual(self.buffer.available(), 12)
+
+    data = []
+    self.assertEqual(self.buffer.read(8, data), 8)
+    self.assertEqual(data, [[8.0, 9.0, 10.0, 11.0, 0.0, 1.0, 2.0, 3.0]])
+
