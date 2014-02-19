18107dcadcd5570af0286a7d0f651535dec2fa8b
Tom Ritchford <tom@swirly.com>
2014-02-19 15:55:30 -0500

    New ConfigValues class and test.

---
+++ code/python/echomesh/util/config/ConfigValues.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from echomesh.base import Config
+
+class ConfigValues(object):
+  def __init__(self, configs=None, values=None, add_client=Config.add_client):
+    for k, v in (values or None).items():
+      if v is not None:
+        assert not hasattr(self, k)
+        setattr(self, k, v)
+
+    self._configs = {}
+    for k, v in (configs or None).items():
+      if not (v is None or hasattr(self, k)):
+        self._configs[k] = v.split('.')
+
+    add_client(self)
+
+  def config_update(self, get):
+    for k, v in self._configs.items():
+      setattr(self, k, get(*v))
+
+
+
+
+
+
---
+++ code/python/echomesh/util/config/ConfigValues_test.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from echomesh.util.config.ConfigValues import ConfigValues
+from echomesh.util.TestCase import TestCase
+
+class ConfigValuesTest(TestCase):
+  def add_client(self, values):
+    def get(*names):
+      if names == ('c',):
+        return 42
+      if names == ('d', 'e'):
+        return 99
+      raise Exception('oops')
+
+    values.config_update(get)
+
+  def setUp(self):
+    self.values = ConfigValues(values={'a': 23, 'b': 'hello'},
+                               configs={'c': 'c', 'd': 'd.e'},
+                               add_client=self.add_client)
+
+  def test_simple(self):
+    self.assertEqual(self.values.a, 23)
+    self.assertEqual(self.values.b, 'hello')
+    self.assertEqual(self.values.c, 42)
+    self.assertEqual(self.values.d, 99)
