a18ecf95155bbea98a8ad235b262fb47d1def741
Tom Ritchford <tom@swirly.com>
2014-02-22 17:03:20 -0500

    start of Scroll code, with first unit tests.

---
+++ code/python/echomesh/color/Scroll_test.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from echomesh.color.Scroll import scroll
+
+from echomesh.util.TestCase import TestCase
+
+class ScrollTest(TestCase):
+  def setUp(self):
+    self.data = [1, 2, 3, 4, 10, 20, 30, 40, 100, 200, 300, 400]
+
+  def doTest(self, dx, dy, expected):
+    self.assertEquals(scroll(self.data, 4, dx, dy, 0), expected)
+
+  def test_empty(self):
+    self.doTest(0, 0, self.data)
+
+  def test_one_right(self):
+    self.doTest(1, 0, [0, 1, 2, 3, 0, 10, 20, 30, 0, 100, 200, 300])
+
+  def test_three_right(self):
+    self.doTest(3, 0, [0, 0, 0, 1, 0, 0, 0, 10, 0, 0, 0, 100])
+
+  def test_four_right(self):
+    self.doTest(4, 0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
