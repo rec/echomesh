0288f777d4cb212abbf80bd42e520607b8d93359
Tom Ritchford <tom@swirly.com>
2014-02-23 12:23:41 -0500

    Tiny changes to scroll.

--- code/python/echomesh/color/Scroll.py
+++ code/python/echomesh/color/Scroll.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-def scroll(items, x, dx, dy, empty=None):
+def _scroll(items, x, dx, dy, empty=None):
   if abs(dx) >= x or abs(dy) >= len(items) / x:
     return [empty] * len(items)
+
   if dx > 0:
     items = items[:]
     for i in xrange(len(items) - 1, -1, -1):
@@ -21,5 +22,9 @@ def scroll(items, x, dx, dy, empty=None):
     items = items[-dy * x:] + [empty] * (-dy * x)
   return items
 
-#def smooth_scroll(colors, x, dx, dy):
+def scroll(colors, x, dx, dy, empty=None, smooth=True):
+  bx, by = int(dx), int(dy)
+  if (not smooth) or (bx == dx and by == dy):
+    return _scroll(colors, x, dx, dy, empty)
+
 
