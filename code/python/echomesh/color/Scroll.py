7ced44ea2588f2e5c4f2fad761918f789dc0a0d0
Tom Ritchford <tom@swirly.com>
2014-02-23 21:23:27 -0500

    Starting to prepare Scroll unit test for changing.

--- code/python/echomesh/color/Scroll.py
+++ code/python/echomesh/color/Scroll.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-def _scroll(items, x, dx, dy, empty=None, wrap=False):
+def _scroll(items, dx, dy, x, empty=None, wrap=False):
   if abs(dx) >= x or abs(dy) >= len(items) / x:
     return [empty] * len(items)
 
@@ -22,7 +22,7 @@ def _scroll(items, x, dx, dy, empty=None, wrap=False):
     items = items[-dy * x:] + [empty] * (-dy * x)
   return items
 
-def scroll(colors, x, dx, dy, empty=None, smooth=True, wrap=False):
+def scroll(colors, dx, dy, x_size, empty=None, smooth=True, wrap=False):
   bx, by = int(dx), int(dy)
   if (not smooth) or (bx == dx and by == dy):
-    return _scroll(colors, x, dx, dy, empty, wrap)
+    return _scroll(colors, dx, dy, x_size, empty, wrap)
--- code/python/echomesh/color/Scroll_test.py
+++ code/python/echomesh/color/Scroll_test.py
@@ -6,65 +6,84 @@ from echomesh.util.TestCase import TestCase
 
 class ScrollTest(TestCase):
   def setUp(self):
-    self.data = [1, 2, 3, 4, 10, 20, 30, 40, 100, 200, 300, 400]
+    self.data = ['red', 'green', 'blue', 'yellow', 'beige', 'khaki',
+                 'olive', 'tan', 'plum', 'teal', 'wheat', 'orchid', ]
 
   def doTest(self, dx, dy, expected):
-    self.assertEquals(scroll(self.data, 4, dx, dy, 0), expected)
+    self.assertEquals(scroll(self.data, dx, dy, 4, empty='black'), expected)
 
   def test_empty(self):
     self.doTest(0, 0, self.data)
 
   def test_one_right(self):
-    self.doTest(1, 0, [0, 1, 2, 3, 0, 10, 20, 30, 0, 100, 200, 300])
+    self.doTest(1, 0, ['black', 'red', 'green', 'blue', 'black', 'beige',
+                       'khaki', 'olive', 'black', 'plum', 'teal', 'wheat', ])
 
   def test_three_right(self):
-    self.doTest(3, 0, [0, 0, 0, 1, 0, 0, 0, 10, 0, 0, 0, 100])
+    self.doTest(3, 0, ['black', 'black', 'black', 'red', 'black', 'black',
+                       'black', 'beige', 'black', 'black', 'black', 'plum', ])
 
   def test_four_right(self):
-    self.doTest(4, 0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    self.doTest(4, 0, ['black', 'black', 'black', 'black', 'black', 'black',
+                       'black', 'black', 'black', 'black', 'black', 'black', ])
 
   def test_five_right(self):
-    self.doTest(5, 0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    self.doTest(5, 0, ['black', 'black', 'black', 'black', 'black', 'black',
+                       'black', 'black', 'black', 'black', 'black', 'black', ])
 
   def test_one_left(self):
-    self.doTest(-1, 0, [2, 3, 4, 0, 20, 30, 40, 0, 200, 300, 400, 0])
+    self.doTest(-1, 0, ['green', 'blue', 'yellow', 'black', 'khaki', 'olive',
+                        'tan', 'black', 'teal', 'wheat', 'orchid', 'black', ])
 
   def test_three_left(self):
-    self.doTest(-3, 0, [4, 0, 0, 0, 40, 0, 0, 0, 400, 0, 0, 0])
+    self.doTest(-3, 0, ['yellow', 'black', 'black', 'black', 'tan', 'black',
+                        'black', 'black', 'orchid', 'black', 'black', 'black', ])
 
   def test_four_left(self):
-    self.doTest(-4, 0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    self.doTest(-4, 0, ['black', 'black', 'black', 'black', 'black', 'black',
+                        'black', 'black', 'black', 'black', 'black', 'black', ])
 
   def test_five_left(self):
-    self.doTest(-5, 0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    self.doTest(-5, 0, ['black', 'black', 'black', 'black', 'black', 'black',
+                        'black', 'black', 'black', 'black', 'black', 'black', ])
 
   def test_one_down(self):
-    self.doTest(0, 1, [0, 0, 0, 0, 1, 2, 3, 4, 10, 20, 30, 40])
+    self.doTest(0, 1, ['black', 'black', 'black', 'black', 'red', 'green',
+                       'blue', 'yellow', 'beige', 'khaki', 'olive', 'tan', ])
 
   def test_two_down(self):
-    self.doTest(0, 2, [0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4])
+    self.doTest(0, 2, ['black', 'black', 'black', 'black', 'black', 'black',
+                       'black', 'black', 'red', 'green', 'blue', 'yellow', ])
 
   def test_three_down(self):
-    self.doTest(0, 3, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    self.doTest(0, 3, ['black', 'black', 'black', 'black', 'black', 'black',
+                       'black', 'black', 'black', 'black', 'black', 'black', ])
 
   def test_four_down(self):
-    self.doTest(0, 4, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    self.doTest(0, 4, ['black', 'black', 'black', 'black', 'black', 'black',
+                       'black', 'black', 'black', 'black', 'black', 'black', ])
 
   def test_one_up(self):
-    self.doTest(0, -1, [10, 20, 30, 40, 100, 200, 300, 400, 0, 0, 0, 0])
+    self.doTest(0, -1, ['beige', 'khaki', 'olive', 'tan', 'plum', 'teal',
+                        'wheat', 'orchid', 'black', 'black', 'black', 'black', ])
 
   def test_two_up(self):
-    self.doTest(0, -2, [100, 200, 300, 400, 0, 0, 0, 0, 0, 0, 0, 0])
+    self.doTest(0, -2, ['plum', 'teal', 'wheat', 'orchid', 'black', 'black',
+                        'black', 'black', 'black', 'black', 'black', 'black', ])
 
   def test_three_up(self):
-    self.doTest(0, -3, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    self.doTest(0, -3, ['black', 'black', 'black', 'black', 'black', 'black',
+                        'black', 'black', 'black', 'black', 'black', 'black', ])
 
   def test_four_up(self):
-    self.doTest(0, -4, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    self.doTest(0, -4, ['black', 'black', 'black', 'black', 'black', 'black',
+                        'black', 'black', 'black', 'black', 'black', 'black', ])
 
   def test_down_right(self):
-    self.doTest(1, 1, [0, 0, 0, 0, 0, 1, 2, 3, 0, 10, 20, 30])
+    self.doTest(1, 1, ['black', 'black', 'black', 'black', 'black', 'red',
+                       'green', 'blue', 'black', 'beige', 'khaki', 'olive', ])
 
   def test_up_left(self):
-    self.doTest(-1, -1, [20, 30, 40, 0, 200, 300, 400, 0, 0, 0, 0, 0])
+    self.doTest(-1, -1, ['khaki', 'olive', 'tan', 'black', 'teal', 'wheat',
+                         'orchid', 'black', 'black', 'black', 'black', 'black', ])
 
