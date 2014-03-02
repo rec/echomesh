bced6c371b6e0bcd8decec9824d7282046f43f50
Tom Ritchford <tom@swirly.com>
2014-03-01 20:45:14 -0500

    Text handles offsets and sizes somewhat better.

--- code/python/echomesh/pattern/Text.py
+++ code/python/echomesh/pattern/Text.py
@@ -11,31 +11,52 @@ from echomesh.util import Log
 LOGGER = Log.logger(__name__)
 _SUFFIX = '.ttf'
 
+def _get_font(fontfile, text, height):
+  new_height = height
+  while True:
+    try:
+      font = ImageFont.truetype(fontfile, new_height)
+    except:
+      raise Exception('Can\'t open font file %s' % fontfile)
+    size = font.getsize(text)
+    if height >= size[1]:
+      return font, size
+    else:
+      new_height -= 1
+
 class Text(Pattern):
-  CONSTANTS =  'font', 'size', 'text'
-  OPTIONAL_CONSTANTS = {'oversample': 4,}
+  CONSTANTS =  'font', 'height', 'text'
+  OPTIONAL_CONSTANTS = {'oversample': 1, 'resample': None}
   PATTERN_COUNT = 0
 
   def _evaluate(self):
     fontfile = self.get('font')
-    height = self.get('size')
+    height = self.get('height')
     oversample = self.get('oversample')
     text = self.get('text')
 
     if not fontfile.endswith(_SUFFIX):
       fontfile += _SUFFIX
 
-      height_over = height * oversample
-    try:
-      font = ImageFont.truetype(fontfile, height_over)
-    except:
-      raise Exception('Can\'t open font file %s' % fontfile)
-    size = font.getsize(text)
+    height_over = height * oversample
+    font, size = _get_font(fontfile, text, height_over)
+    offset = font.getoffset(text)
     image = Image.new('RGBA', size)
     draw = ImageDraw.Draw(image)
-    draw.text((0, 0), text=text, font=font)
+    draw.text((-offset[0], -offset[1]), text=text, font=font)
 
-    columns = int(math.ceil(size[0] / oversample))
-    rows = int(math.ceil(size[1] / oversample))
-    image = image.resize((columns, rows), resample=Image.ANTIALIAS)
-    return cechomesh.ColorList(image, columns=columns)
+    width = size[0]
+    image.show()
+    if oversample > 1:
+      width = int(math.ceil(width / oversample))
+      if resample:
+        try:
+          resample = getattr(Image, resample.upper())
+        except:
+          raise Exception("Don't understand resample=%s" % resample)
+      else:
+        resample = Image.BICUBIC
+      image = image.resize((width, height), resample=resample)
+      image.show()
+
+    return cechomesh.ColorList(image, columns=width)
