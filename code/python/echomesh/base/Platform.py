2f08870efba7c62bf86e0085196dbe946ec7232a
Tom Ritchford <tom@swirly.com>
2014-03-16 16:32:59 -0400

    Change Platform to explicitly detect the Raspberry Pi.

--- code/python/echomesh/base/Platform.py
+++ code/python/echomesh/base/Platform.py
@@ -4,17 +4,28 @@ import platform
 
 PLATFORM = platform.system().lower()
 
+DEBIAN = 'debian'
+MAC = 'darwin'
+RASPBERRY_PI = 'raspberry pi'
+UBUNTU = 'ubuntu'
+WINDOWS = 'windows'
+
 IS_LINUX = (PLATFORM == 'linux')
 
 if IS_LINUX:
   PLATFORM = platform.linux_distribution()[0].lower()
+  if PLATFORM == DEBIAN:
+    try:
+      with open('/proc/cpuinfo') as f:
+        for line in f:
+          line = line.strip()
+          if line.startswith('Hardware') and line.endswith('BCM2708'):
+            PLATFORM = RASPBERRY_PI
+            break
+    except:
+      pass
 
-DEBIAN = 'debian'
-MAC = 'darwin'
-UBUNTU = 'ubuntu'
-WINDOWS = 'windows'
-
-LEGAL_PLATFORMS = DEBIAN, MAC, UBUNTU, WINDOWS
+LEGAL_PLATFORMS = DEBIAN, MAC, RASPBERRY_PI, UBUNTU, WINDOWS
 
 assert PLATFORM in LEGAL_PLATFORMS, "Don't understand platform %s" % PLATFORM
 
