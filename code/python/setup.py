92b3f057f1926d812192a9a05b64dfdac2eb0bcf
Tom Ritchford <tom@swirly.com>
2014-11-30 12:07:15 -0500

    Add initial setup.py

---
+++ code/python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='echomesh',
+      version='0.7',
+      description='A system for creating distributed art projects',
+      author='Tom Ritchford',
+      author_email='tom@swirly.com',
+      url='https://github.com/rec/echomesh/',
+      packages=['echomesh'],
+     )
